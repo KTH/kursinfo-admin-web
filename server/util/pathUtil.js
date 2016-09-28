ddc0598ca4d1c6eca2921943ca7bcdb77d23db6f
Jonas Lantto <jonas.e.lantto@gmail.com>
2016-09-28 16:07:54 +0200

    linux debugging and standard for path resolving

--- server/util/pathUtil.js
+++ server/util/pathUtil.js
@@ -1,10 +1,23 @@
 'use strict'
 
 module.exports = {
-  addParamToPath: _addParamToPath
+  addParamToPath: _addParamToPath,
+  resolve: _resolve
 }
 
+// Adds string to url path. DEPRECATED: use resolve instead
 function _addParamToPath (path, param, value) {
   const paramToReplace = ':' + param
   return path.replace(paramToReplace, value)
 }
+
+// Adds parameters to url
+function _resolve (uri, params, skipEncode) {
+  Object.keys(params).forEach((key) => {
+    const part = skipEncode ? params[key] : encodeURIComponent(params[key])
+    uri = uri.replace(new RegExp(':' + key, 'gi'), part)
+  })
+
+  return uri
+}
+
