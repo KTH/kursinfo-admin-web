f1e02ea2b97b682c5360fa76ef58260f6b6e605e
Ove Sandfeldt <ovesan@kth.se>
2016-09-30 15:14:42 +0200

    Add new config

--- config/secretSettings.js
+++ config/secretSettings.js
@@ -1,46 +1,50 @@
+'use strict'
+
+const getEnv = require('kth-node-configuration').getEnv
+
 module.exports = {
-  useSsl: process.env.SERVER_USE_SSL === 'true',
-  hostUrl: process.env.SERVER_HOST_URL,
-  port: process.env.SERVER_PORT,
+  useSsl: getEnv('SERVER_USE_SSL',false),
+  ssl: {
+    pfx: getEnv('SERVER_CERT_FILE', '/certs/localhost.p12'),
+    passphrase: getEnv('SERVER_CERT_PASSPHRASE', '/certs/localhost.pass')
+  },
+  hostUrl: getEnv('SERVER_HOST_URL'),
+  port: getEnv('SERVER_PORT'),
   secure: {
-    sessionSecret: process.env.SESSION_SECRET,
+    sessionSecret: getEnv('SESSION_SECRET'),
     apiKey: {
-      sampleApi: process.env.NODE_API_KEY
+      sampleApi: getEnv('NODE_API_KEY')
     },
     ldap: {
       // in dev, set this to true to bypass any authorization of the user
       bypassUserAuthorization: false,
-      uri: process.env.LDAP_URI,
-      base: process.env.LDAP_BASE,
-      filter: process.env.LDAP_FILTER,
-      username: process.env.LDAP_USERNAME,
-      password: process.env.LDAP_PASSWORD
+      uri: getEnv('LDAP_URI'),
+      base: getEnv('LDAP_BASE'),
+      filter: getEnv('LDAP_FILTER'),
+      username: getEnv('LDAP_USERNAME'),
+      password: getEnv('LDAP_PASSWORD')
     }
   },
-  ssl: {
-    passphrase: '',
-    pfx: ''
-  },
   cas: {
-    ssoBaseURL: process.env.CAS_SSO_URI
+    ssoBaseURL: getEnv('CAS_SSO_URI')
   },
   nodeApi: {
     sampleApi: {
       https: false,
       port: 3001,
-      host: process.env.NODE_API_SAMPLE_HOST,
+      host: getEnv('NODE_API_SAMPLE_HOST'),
       proxyBasePath: '/api/node'
     }
   },
   blockApi: {
-    blockUrl: process.env.BLOCK_API_URL
+    blockUrl: getEnv('BLOCK_API_URL')
   },
   logging: {
     log: {
-      level: 'debug'
+      level: getEnv('LOGGING_LEVEL', 'info')
     },
     accessLog: {
-      useAccessLog: true
+      useAccessLog: getEnv('LOGGING_ACCESS_LOG', true)
     }
   },
   clientLogging: {
@@ -49,22 +53,25 @@ module.exports = {
   cache: {
     cortinaBlock: {
       redis: {
-        host: 'localhost',
-        port: 6379
+        host: getEnv('CACHE_CORTINA_BLOCK_HOST', 'localhost'),
+        port: getEnv('CACHE_CORTINA_BLOCK_PORT', 6379)
       }
     }
   },
   cssTranspiler: {
-    force: true,
-    debug: true
+    force: getEnv('CSS_TRANSPILER_FORCE', true),
+    debug: getEnv('CSS_TRANSPILER_DEBUG', true)
   },
   session: {
-    key: 'node-web.sid',
-    useRedis: false,
+    key: getEnv('SESSION_KEY', 'node-web.sid'),
+    useRedis: getEnv('SESSION_USE_REDIS', false),
     sessionOptions: {
       // do not set session secret here!!
-      cookie: { secure: false }
+      cookie: { secure: getEnv('SESSION_SECURE_COOKIE', false) }
     },
-    redisOptions: {}
+    redisOptions: {
+      host: getEnv('CACHE_CORTINA_BLOCK_HOST', 'localhost'),
+      port: getEnv('CACHE_CORTINA_BLOCK_PORT', 6379)
+    }
   }
 }
