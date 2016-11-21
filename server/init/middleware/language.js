af2c939e53db8cc8289ce512ef387bf8d47c0443
jhsware <sebastian@urbantalk.se>
2016-11-21 18:31:00 +0100

    Use locale to get language, only set language if explicitly passed

--- server/init/middleware/language.js
+++ server/init/middleware/language.js
@@ -8,15 +8,14 @@ const language = require('../../util/language')
  * Middleware that checks for l-flag and stores its value in the session
  */
 function _setLanguage (req, res, next) {
-  if (/^\/static\/.*/.test(req.url)) {
-    // don't check language on static routes
-    next()
-    return
+  // Set locale if not already done by other middleware
+  // You can customise behaviour by adding middleware before this one
+  if (!res.locals.locale) {
+    language.init(req, res, req.query[ 'l' ])
   }
-
-  language.init(req, res, req.query[ 'l' ])
-
   next()
 }
 
-server.use(config.full.proxyPrefixPath.uri, _setLanguage)
+// Match all routes that aren't under the static directory
+const langRouteRegex = new RegExp('^' + config.full.proxyPrefixPath.uri + '/(?!static).*$', 'g')
+server.use(langRouteRegex, _setLanguage)
--- server/util/language.js
+++ server/util/language.js
@@ -6,46 +6,41 @@
  */
 
 const log = require('kth-node-log')
+const locale = require('locale')
 const cookieName = 'language'
 const validLanguages = [ 'sv', 'en' ]
 const defaultLanguage = validLanguages[ 0 ]
 
-function _isValid (lang) {
-  return validLanguages.indexOf(lang) >= 0
-}
-
 /**
- * Initialize language for the current user session (cookie).
+ * Initialize locale and language for the current user session (respects cookie: language).
  */
 function _init (req, res, newLang) {
-  let lang = req.cookies[ cookieName ]
+  let lang = newLang || req.cookies[ cookieName ]
+  var chosenLocale
 
-  if (newLang && _isValid(newLang)) {
-    log.debug(`Language set from external source: '${newLang}'`)
-    res.cookie(cookieName, newLang)
-    lang = newLang
-  } else if (!lang || !_isValid(lang)) {
-    log.debug(`Language not set or invalid, setting default: '${defaultLanguage}'`)
-    res.cookie(cookieName, defaultLanguage)
-    lang = defaultLanguage
+  if (lang) {
+    let locales = new locale.Locales(lang, defaultLanguage)
+    chosenLocale = locales.best(validLanguages)
+  } else {
+    let locales = new locale.Locales(req.headers['accept-language'], defaultLanguage)
+    chosenLocale = locales.best(validLanguages)
   }
 
-  res.locals.language = lang
+  // If we got an explicit language we set the language cookie
+  if (newLang) {
+    res.cookie(cookieName, chosenLocale.language)
+  }
+
+  res.locals.locale = chosenLocale
+  // Backwards compatibility only in case someone accessed the prop directly:
+  res.locals.language = chosenLocale.getLanguage
 }
 
 /**
  * Helper that gets the current language from the session
  */
 function _getLanguage (res) {
-  const lang = res.locals.language
-
-  if (lang && _isValid(lang)) {
-    log.debug(`Language found: '${lang}'`)
-    return lang
-  }
-
-  log.debug(`Language not set, getting default: '${defaultLanguage}'`)
-  return defaultLanguage
+  return res.locals.chosenLocale && res.locals.chosenLocale.language
 }
 
 module.exports = {
