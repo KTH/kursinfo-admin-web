5a1ffe8707d5a578226d387137c307d3d4b0bb3a
jhsware <sebastian@urbantalk.se>
2016-11-21 19:27:07 +0100

    Bugfix

--- server/util/language.js
+++ server/util/language.js
@@ -10,6 +10,7 @@ const locale = require('locale')
 const cookieName = 'language'
 const validLanguages = [ 'sv', 'en' ]
 const defaultLanguage = validLanguages[ 0 ]
+const supportedLocales = new locale.Locales(validLanguages, defaultLanguage)
 
 /**
  * Initialize locale and language for the current user session (respects cookie: language).
@@ -20,10 +21,10 @@ function _init (req, res, newLang) {
 
   if (lang) {
     let locales = new locale.Locales(lang, defaultLanguage)
-    chosenLocale = locales.best(validLanguages)
+    chosenLocale = locales.best(supportedLocales)
   } else {
     let locales = new locale.Locales(req.headers['accept-language'], defaultLanguage)
-    chosenLocale = locales.best(validLanguages)
+    chosenLocale = locales.best(supportedLocales)
   }
 
   // If we got an explicit language we set the language cookie
@@ -34,16 +35,20 @@ function _init (req, res, newLang) {
   res.locals.locale = chosenLocale
   // Backwards compatibility only in case someone accessed the prop directly:
   res.locals.language = chosenLocale.getLanguage
+
+  return chosenLocale
 }
 
 /**
  * Helper that gets the current language from the session
  */
 function _getLanguage (res) {
-  return res.locals.chosenLocale && res.locals.chosenLocale.language
+  return res.locals.locale && res.locals.locale.language
 }
 
 module.exports = {
   getLanguage: _getLanguage,
-  init: _init
+  init: _init,
+  validLanguages: validLanguages,
+  defaultLanguage: defaultLanguage
 }
