2fe4e98ba8ca823a5903316d125fa9e1e23fdb81
Ove Sandfeldt <ovesan@kth.se>
2016-12-13 13:43:25 +0100

    Configure logging without any preset console logger

--- server/init/logging/logging.js
+++ server/init/logging/logging.js
@@ -9,38 +9,14 @@ const fs = require('fs')
 const configuration = config.full.logging
 const environment = config.env
 
-const logConfiguration = {
+let logConfiguration = {
   name: packageFile.name,
   app: packageFile.name,
   env: environment,
   level: configuration.log.level,
+  console: configuration.console,
+  stdout: configuration.stdout,
   src: configuration.src
-}
-
-if (configuration.logstash) {
-  const certs = configuration.logstash.caCerts.map((cert) => {
-    if (cert.indexOf('/') === 0) {
-      return fs.readFileSync(cert)
-    }
-
-    return fs.readFileSync(path.join(__dirname, '/../../../config/', cert))
-  })
-
-  logConfiguration.logstash = {
-    enabled: true,
-    tlsOptions: {
-      host: configuration.logstash.host,
-      port: configuration.logstash.port,
-      ca: certs
-    },
-    lumberjackOptions: {
-      maxQueueSize: configuration.logstash.maxQueueSize
-    }
-  }
-} else {
-  logConfiguration.console = {
-    enabled: true
-  }
 }
 
 log.init(logConfiguration)
