2ae9829d7086d5cb43d61115fddcdb9a7fecec64
jhsware <sebastian@urbantalk.se>
2017-05-29 10:43:22 +0200

    Jonas new breadcrumbs setup

--- server/views/helpers/index.js
+++ server/views/helpers/index.js
@@ -32,7 +32,7 @@ registerHeaderContentHelper({
  * packaged helpers in https://github.com/KTH/kth-node-web-common/tree/master/lib/handlebars/helpers
  * Those only need to be required. Docs embedded in source.
  */
-require('kth-node-web-common/lib/handlebars/helpers/breadcrumbs')
+require('kth-node-web-common/lib/handlebars/helpers/breadcrumbs')(config.hostUrl, 'host_name', '/node', 'site_name')
 require('kth-node-web-common/lib/handlebars/helpers/contentedit')
 
 const i18n = require('../../../i18n')
