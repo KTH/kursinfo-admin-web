e03dfc44e8f87e65b3f1b8c0c692869c6dcd1353
Patric Jansson <paddy@kth.se>
2018-02-16 14:57:16 +0100

    Added customHeaders

--- server/api.js
+++ server/api.js
@@ -9,7 +9,10 @@ const opts = {
   log: log,
   redis: redis,
   timeout: 30000,
-  checkAPIs: true // performs api-key checks against the apis, if a "required" check fails, the app will exit. Required apis are specified in the config
+  checkAPIs: true, // performs api-key checks against the apis, if a "required" check fails, the app will exit. Required apis are specified in the config
+  customHeaders: {
+    'User-Agent': 'KTH'
+  }
 }
 
 module.exports = connections.setup(config.nodeApi, config.apiKey, opts)
