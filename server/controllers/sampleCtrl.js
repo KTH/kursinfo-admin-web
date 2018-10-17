b277b2b9334f58e951915fc5d8ffd1836b4dc5ab
Elena Rakhimova <elenara@kth.se>
2018-10-17 12:07:29 +0200

    DEBUG Again

--- server/controllers/sampleCtrl.js
+++ server/controllers/sampleCtrl.js
@@ -11,14 +11,15 @@ module.exports = {
 
 function * getIndex (req, res, next) {
   try {
-    const client = api.nodeApi.client
-    const paths = api.nodeApi.paths
-    const resp = yield client.getAsync(client.resolve(paths.getDataById.uri, { id: '123' }), { useCache: true })
+    // const client = api.nodeApi.client
+    // const paths = api.nodeApi.paths
+    // const resp = yield client.getAsync(client.resolve(paths.getDataById.uri, { id: '123' }), { useCache: true })
 
     res.render('sample/index', {
-      debug: 'debug' in req.query,
-      data: resp.statusCode === 200 ? safeGet(() => { return resp.body.name }) : '',
-      error: resp.statusCode !== 200 ? safeGet(() => { return resp.body.message }) : ''
+      data: "Hello!!"
+      //debug: 'debug' in req.query,
+      // // data: resp.statusCode === 200 ? safeGet(() => { return resp.body.name }) : '',
+      // error: resp.statusCode !== 200 ? safeGet(() => { return resp.body.message }) : ''
     })
   } catch (err) {
     log.error('Error in getIndex', { error: err })
