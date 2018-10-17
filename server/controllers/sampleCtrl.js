202345ae0ada338609c4befca88a91c577cf7952
Elena Rakhimova <elenara@kth.se>
2018-10-17 15:18:43 +0200

    removed debig stuff

--- server/controllers/sampleCtrl.js
+++ server/controllers/sampleCtrl.js
@@ -11,15 +11,14 @@ module.exports = {
 
 function * getIndex (req, res, next) {
   try {
-    // const client = api.nodeApi.client
-    // const paths = api.nodeApi.paths
-    // const resp = yield client.getAsync(client.resolve(paths.getDataById.uri, { id: '123' }), { useCache: true })
+    const client = api.nodeApi.client
+    const paths = api.nodeApi.paths
+    const resp = yield client.getAsync(client.resolve(paths.getDataById.uri, { id: '123' }), { useCache: true })
 
     res.render('sample/index', {
-      data: "Hello!!"
-      //debug: 'debug' in req.query,
-      // // data: resp.statusCode === 200 ? safeGet(() => { return resp.body.name }) : '',
-      // error: resp.statusCode !== 200 ? safeGet(() => { return resp.body.message }) : ''
+      debug: 'debug' in req.query,
+      data: resp.statusCode === 200 ? safeGet(() => { return resp.body.name }) : '',
+      error: resp.statusCode !== 200 ? safeGet(() => { return resp.body.message }) : ''
     })
   } catch (err) {
     log.error('Error in getIndex', { error: err })
