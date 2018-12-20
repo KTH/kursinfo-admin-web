2f99d242acf999a1e5410742ca418275654c68b4
Elena Rakhimova <elenara@kth.se>
2018-12-20 15:54:23 +0100

    removed old files

--- server/controllers/courseCtrl.js
+++ server/controllers/courseCtrl.js
@@ -15,14 +15,13 @@ const serverConfig = require('../configuration').server
 
 let { appFactory, doAllAsyncBefore } = require('../../dist/js/server/app.js')
 
-
 module.exports = {
   getIndex: getIndex
 }
 
 const paths = require('../server').getPaths()
 
- async function  getIndex (req, res, next) {
+function getIndex (req, res, next) {
   if (process.env['NODE_ENV'] === 'development') {
     delete require.cache[require.resolve('../../dist/js/server/app.js')]
     const tmp = require('../../dist/js/server/app.js')
@@ -38,31 +37,31 @@ const paths = require('../server').getPaths()
    // const resp = yield client.getAsync(client.resolve(paths.getDataById.uri, { id: '123' }), { useCache: true })
 
   // Render inferno app
-  const context = {}
-  const renderProps = createElement(StaticRouter, {
-    location: req.url,
-    context
-  }, appFactory())
-
-  await renderProps.props.children.props.routerStore.getCourseInformation(courseCode, lang)
-  renderProps.props.children.props.routerStore.setBrowserConfig(browserConfig, paths, serverConfig.hostUrl)
-  renderProps.props.children.props.routerStore.__SSR__setCookieHeader(req.headers.cookie)
+    const context = {}
+    const renderProps = createElement(StaticRouter, {
+      location: req.url,
+      context
+    }, appFactory())
+    
+    await renderProps.props.children.props.routerStore.getCourseInformation(courseCode, lang)
+    renderProps.props.children.props.routerStore.setBrowserConfig(browserConfig, paths, serverConfig.hostUrl)
+    renderProps.props.children.props.routerStore.__SSR__setCookieHeader(req.headers.cookie)
 
   // console.log("!!renderProps!!", renderProps)
-  await doAllAsyncBefore({
-    pathname: req.originalUrl,
-    query: (req.originalUrl === undefined || req.originalUrl.indexOf('?') === -1) ? undefined : req.originalUrl.substring(req.originalUrl.indexOf('?'), req.originalUrl.length),
-    routerStore: renderProps.props.children.props.routerStore,
-    routes: renderProps.props.children.props.children.props.children.props.children
-  })
-  const html = renderToString(renderProps)
+    await doAllAsyncBefore({
+      pathname: req.originalUrl,
+      query: (req.originalUrl === undefined || req.originalUrl.indexOf('?') === -1) ? undefined : req.originalUrl.substring(req.originalUrl.indexOf('?'), req.originalUrl.length),
+      routerStore: renderProps.props.children.props.routerStore,
+      routes: renderProps.props.children.props.children.props.children.props.children
+    })
+    const html = renderToString(renderProps)
 
     res.render('course/index', {
       debug: 'debug' in req.query,
-      html:html
-    ,initialState: JSON.stringify(hydrateStores(renderProps))
-      //data: resp.statusCode === 200 ? safeGet(() => { return resp.body.name }) : '',
-      //error: resp.statusCode !== 200 ? safeGet(() => { return resp.body.message }) : ''
+      html: html,
+      initialState: JSON.stringify(hydrateStores(renderProps))
+      // data: resp.statusCode === 200 ? safeGet(() => { return resp.body.name }) : '',
+      // error: resp.statusCode !== 200 ? safeGet(() => { return resp.body.message }) : ''
     })
   } catch (err) {
     log.error('Error in getIndex', { error: err })
@@ -72,7 +71,7 @@ const paths = require('../server').getPaths()
 
 function hydrateStores (renderProps) {
   // This assumes that all stores are specified in a root element called Provider
-  
+
   const props = renderProps.props.children.props
   const outp = {}
   for (let key in props) {
@@ -80,6 +79,6 @@ function hydrateStores (renderProps) {
       outp[key] = encodeURIComponent(JSON.stringify(toJS(props[key], true)))
     }
   }
-  console.log("hydrateStores", outp)
+  console.log('hydrateStores', outp)
   return outp
 }
