a0aa99a71e6734af1fb317eb9bc134a59554c332
Elena Rakhimova <elenara@kth.se>
2019-01-16 16:45:37 +0100

    Added server path with requireRole and without

--- server/server.js
+++ server/server.js
@@ -184,11 +184,9 @@ server.use('/', systemRoute.getRouter())
 
 // App routes
 const appRoute = AppRouter()
-appRoute.get('course.getDescription', config.proxyPrefixPath.uri + '/:courseCode', getServerGatewayLogin(), /* requireRole('isCourseResponsible'),*/ SellingInfo.getDescription)
-appRoute.get('course.getDescription', config.proxyPrefixPath.uri + '/_admin/:courseCode', getServerGatewayLogin(), requireRole('isAdmin'), SellingInfo.getDescription)
-appRoute.get('course.getDescription', config.proxyPrefixPath.uri + '/_me/:courseCode', getServerGatewayLogin(), requireRole('isMe'), SellingInfo.getDescription)
-appRoute.post('course.updateDescription', config.proxyPrefixPath.uri + '/api/:courseCode/', getServerGatewayLogin(), /* requireRole('isCourseResponsible'),*/ SellingInfo.updateDescription)
-// appRoute.get('course.reviewDescription', config.proxyPrefixPath.uri + '/:courseCode', serverLogin, SellingInfo.reviewDescription)
+appRoute.get('course.getDescription', config.proxyPrefixPath.uri + '/:courseCode', getServerGatewayLogin(), requireRole('isCourseResponsible', 'isExaminator'), SellingInfo.getDescription)
+appRoute.get('course.getDescription', config.proxyPrefixPath.uri + '/a/:courseCode', getServerGatewayLogin(), /* requireRole('isCourseResponsible', 'isExaminator'),*/ SellingInfo.getDescription)
+appRoute.post('course.updateDescription', config.proxyPrefixPath.uri + '/api/:courseCode/', getServerGatewayLogin(), /* requireRole('isCourseResponsible', 'isExaminator'),*/ SellingInfo.updateDescription)
 appRoute.get('system.gateway', config.proxyPrefixPath.uri + '/gateway', getServerGatewayLogin('/'), requireRole('isAdmin'), SellingInfo.getDescription)
 server.use('/', appRoute.getRouter())
 
