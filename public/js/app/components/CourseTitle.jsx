e0e8c7402da5df0c747448eeafc12b8dd75a24f2
Elena Rakhimova <elenara@kth.se>
2019-01-25 14:55:10 +0100

    Changed format and styling of title

--- public/js/app/components/CourseTitle.jsx
+++ public/js/app/components/CourseTitle.jsx
@@ -1,19 +1,35 @@
 import { Component } from 'inferno'
-import i18n from "../../../../i18n"
+import i18n from '../../../../i18n'
+import Button from 'inferno-bootstrap/lib/Button'
+import { EMPTY } from '../util/constants'
+
 
 class CourseTitle extends Component {
+  constructor (props) {
+    super(props)
+    this.closeEdit = this.closeEdit.bind(this)
+  }
+
+  closeEdit () {
+    event.preventDefault()
+    window.open(`/student/kurser/kurs/${this.props.courseTitleData.course_code}?lang=${this.props.language}`)
+    // window.open(`/admin/kurser/kurs/${this.props.courseTitleData.course_code}?lang=${language}`)
+  }
+
   render () {
     const title = this.props.courseTitleData
+    title.course_credits = title.course_credits !== EMPTY && title.course_credits.toString().indexOf('.') < 0 ? title.course_credits + '.0' : title.course_credits
     return (
-      <div id="course-title" className="col">
-        <h1><span property="aiiso:code">{title.course_code}</span>
-        <span property="teach:courseTitle"> {title.course_title},</span>
-        <span content={title.course_credits} datatype="xsd:decimal" property="teach:ects"> {title.course_credits} {this.props.language === 0 ? " credits" : " hp"} </span>
+      <div id='course-title' className='courseTitle col'>
+        <h1><span property='aiiso:code'>{title.course_code}</span>
+          <span property='teach:courseTitle'> {title.course_title}</span>
+          <span content={title.course_credits} datatype='xsd:decimal' property='teach:ects'> {this.props.language === 'en' ? title.course_credits : title.course_credits.toString().replace('.', ',')}&nbsp;{this.props.language === 'en' ? 'credits' : 'hp'} </span>
         </h1>
-        <h2 className="secondTitle">
-          <span property="teach:courseTitle">{title.course_other_title}</span>
-        </h2>
-      </div> 
+        <p property='teach:courseTitle'>{title.course_other_title}</p>
+        <span className='Header--Button'>
+          <Button className='editButton' color='secondary' onClick={this.closeEdit} id={title.course_code}><i className='icon-edit'></i> Go to student view </Button>
+        </span>
+      </div>
     )
   }
 }
