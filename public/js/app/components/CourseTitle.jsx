ad6bfc8d88411690ee4c9d9e3783cf0645a5c538
Elena Rakhimova <elenara@kth.se>
2019-01-29 18:08:11 +0100

    Remove exit button from course title and removed old

--- public/js/app/components/CourseTitle.jsx
+++ public/js/app/components/CourseTitle.jsx
@@ -1,19 +1,8 @@
 import { Component } from 'inferno'
 import i18n from '../../../../i18n'
-import Button from 'inferno-bootstrap/lib/Button'
 import { EMPTY } from '../util/constants'
 
 class CourseTitle extends Component {
-  constructor (props) {
-    super(props)
-    this.closeEdit = this.closeEdit.bind(this)
-  }
-
-  closeEdit (event) {
-    event.preventDefault()
-    window.open(`/student/kurser/kurs/${this.props.courseTitleData.course_code}?lang=${this.props.language}`)
-    // window.open(`/admin/kurser/kurs/${this.props.courseTitleData.course_code}?lang=${language}`)
-  }
 
   render () {
     const title = this.props.courseTitleData
@@ -26,9 +15,6 @@ class CourseTitle extends Component {
           <span content={title.course_credits} datatype='xsd:decimal' property='teach:ects'> {this.props.language === 'en' ? title.course_credits : title.course_credits.toString().replace('.', ',')}&nbsp;{this.props.language === 'en' ? 'credits' : 'hp'} </span>
         </h1>
         <p property='teach:courseTitle'>{title.course_other_title}</p>
-        <span className='Header--Button'>
-          <Button className='editButton' color='secondary' onClick={this.closeEdit} id={title.course_code}><i className='icon-edit'></i> {i18n.messages[language].sellingTextButtons.button_student}</Button>
-        </span>
       </div>
     )
   }
