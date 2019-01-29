cc0caf66301b4859efd7ee1407b61cb7e6b05555
Elena Rakhimova <elenara@kth.se>
2019-01-29 13:41:48 +0100

    Make course title translatable

--- public/js/app/components/CourseTitle.jsx
+++ public/js/app/components/CourseTitle.jsx
@@ -3,14 +3,13 @@ import i18n from '../../../../i18n'
 import Button from 'inferno-bootstrap/lib/Button'
 import { EMPTY } from '../util/constants'
 
-
 class CourseTitle extends Component {
   constructor (props) {
     super(props)
     this.closeEdit = this.closeEdit.bind(this)
   }
 
-  closeEdit () {
+  closeEdit (event) {
     event.preventDefault()
     window.open(`/student/kurser/kurs/${this.props.courseTitleData.course_code}?lang=${this.props.language}`)
     // window.open(`/admin/kurser/kurs/${this.props.courseTitleData.course_code}?lang=${language}`)
@@ -19,6 +18,7 @@ class CourseTitle extends Component {
   render () {
     const title = this.props.courseTitleData
     title.course_credits = title.course_credits !== EMPTY && title.course_credits.toString().indexOf('.') < 0 ? title.course_credits + '.0' : title.course_credits
+    const language = this.props.language === 'en' ? 0 : 1
     return (
       <div id='course-title' className='courseTitle col'>
         <h1><span property='aiiso:code'>{title.course_code}</span>
@@ -27,7 +27,7 @@ class CourseTitle extends Component {
         </h1>
         <p property='teach:courseTitle'>{title.course_other_title}</p>
         <span className='Header--Button'>
-          <Button className='editButton' color='secondary' onClick={this.closeEdit} id={title.course_code}><i className='icon-edit'></i> Go to student view </Button>
+          <Button className='editButton' color='secondary' onClick={this.closeEdit} id={title.course_code}><i className='icon-edit'></i> {i18n.messages[language].sellingTextButtons.button_student}</Button>
         </span>
       </div>
     )
