e5b0e40f4cae72dcd58703f93fa5719b71aaf3e7
Elena Rakhimova <elenara@kth.se>
2019-02-01 15:33:29 +0100

    better readability of code

--- public/js/app/components/CourseTitle.jsx
+++ public/js/app/components/CourseTitle.jsx
@@ -7,12 +7,13 @@ class CourseTitle extends Component {
   render () {
     const title = this.props.courseTitleData
     title.course_credits = title.course_credits !== EMPTY && title.course_credits.toString().indexOf('.') < 0 ? title.course_credits + '.0' : title.course_credits
-    const language = this.props.language === 'en' ? 0 : 1
     return (
       <div id='course-title' className='courseTitle col'>
         <h1><span property='aiiso:code'>{title.course_code}</span>
           <span property='teach:courseTitle'> {title.course_title}</span>
-          <span content={title.course_credits} datatype='xsd:decimal' property='teach:ects'> {this.props.language === 'en' ? title.course_credits : title.course_credits.toString().replace('.', ',')}&nbsp;{this.props.language === 'en' ? 'credits' : 'hp'} </span>
+          <span content={title.course_credits} datatype='xsd:decimal' property='teach:ects'>
+            {this.props.language === 'en' ? title.course_credits : title.course_credits.toString().replace('.', ',')}&nbsp;{this.props.language === 'en' ? 'credits' : 'hp'}
+          </span>
         </h1>
         <p property='teach:courseTitle'>{title.course_other_title}</p>
       </div>
