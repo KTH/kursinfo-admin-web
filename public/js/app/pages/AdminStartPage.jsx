3723acf093b07654cd5babe325dd4f751b84c7e1
Elena Rakhimova <elenara@kth.se>
2019-02-08 18:07:41 +0100

    Removed functions and added link instead of button functions

--- public/js/app/pages/AdminStartPage.jsx
+++ public/js/app/pages/AdminStartPage.jsx
@@ -14,78 +14,76 @@ import CardBody from 'inferno-bootstrap/lib/Card/CardBody'
 import CardTitle from 'inferno-bootstrap/lib/Card/CardTitle'
 import CardText from 'inferno-bootstrap/lib/Card/CardText'
 import CardFooter from 'inferno-bootstrap/lib/Card/CardFooter'
+import CardLink from 'inferno-bootstrap/lib/Card/CardLink'
+import {Link} from 'inferno-router'
+import Alert from 'inferno-bootstrap/lib/Alert'
 
 @inject(['adminStore']) @observer
 class AdminStartPage extends Component {
-  constructor (props) {
-    super(props)
-    this.state = {
-      isSellingText: false,
-      isCoursePM: false,
-      isCourseDev: false
-    }
-    this.doStartSellingText = this.doStartSellingText.bind(this)
-    this.doStartCoursePM = this.doStartCoursePM.bind(this)
-    this.doStartCourseDev = this.doStartCourseDev.bind(this)
-    this.doExit = this.doExit.bind(this)
-  }
-
-  doExit (event) {
-    event.preventDefault()
-    this.setState({
-      sellingText: this.props.adminStore.sellingText,
-      editDescription: false,
-      enteredEditMode: false,
-      hasDoneSubmit: false,
-      errorMsg: false
-    })
-    console.log('didExit')
-  }
 
   render ({adminStore}) {
     const courseAdminData = adminStore['courseAdminData']
-
+    const lang = courseAdminData.lang === 'en' ? 0 : 1
+    const courseCode = courseAdminData.courseTitleData.course_code
+    console.log('windowddd', this.props.location.data)
     return (
       <div key='kursinfo-container' className='kursinfo-main-page col' >
         {/* ---COURSE TITEL--- */}
         <CourseTitle key='title'
           courseTitleData={courseAdminData.courseTitleData}
           language={courseAdminData.lang}
-            />
-
-          {/* ---IF in edit mode or not--- */}
-
-        {this.state.editDescription === true ? (
-          <div className='AdminPage--EditSomething col'>
-          </div>
-        ) : (
-          <div className='AdminPage--ShowMenu row'>
+        />
+        {this.props.location.data === 'success' ?
+          <Alert color='success' aria-live='polite'>
+            {i18n.messages[lang].alertMessages.success}
+          </Alert>
+        : ''
+        }
+        <div>
+          <span className='Header--Button'>
+            <a href={`/student/kurser/kurs/${courseCode}?l=${courseAdminData.lang}`} class='link-back'>{i18n.messages[lang].sellingTextButtons.button_course_info}</a>
+          </span>
+          <span className='AdminPage--ShowDescription row'>
             <Card className='KursInfo--SellingText'>
               <CardBody>
-                <CardTitle>Kurssäljande information</CardTitle>
-                <CardText>Lägg till kurssäljande information för att tydligare förklare varför studenter behöver den kursen</CardText>
+                <CardTitle>{i18n.messages[lang].startCards.sellingText_hd}</CardTitle>
+                <CardText>{i18n.messages[lang].startCards.sellingText_desc}</CardText>
                 {/* <CardText><TextBlock text={this.state.sellingText} /></CardText> */}
               </CardBody>
-              <CardFooter className='text-right'><Button onClick={this.doStartSellingText} color='primary'>Lägg till kortbeskrivning</Button></CardFooter>
+              <CardFooter className='text-right'>
+                <a href={`/admin/kurser/kurs/edit/${courseCode}?l=${courseAdminData.lang}`} className='btn btn-primary'>{i18n.messages[lang].startCards.sellingText_btn}</a>
+                {/* <Link to={{ pathname: `/admin/kurser/kurs/edit/${courseCode}?l=${courseAdminData.lang}`,
+                data: 'hello'
+                }} className='btn btn-primary' onClick={this.doStartSellingText}>{i18n.messages[lang].startCards.sellingText_btn}</Link> */}
+                {/* <Button onClick={this.doStartSellingText} color='primary'>{i18n.messages[lang].startCards.sellingText_btn}</Button> */}
+              </CardFooter>
             </Card>
             <Card>
               <CardBody>
-                <CardTitle>Kurs-PM</CardTitle>
-                <CardText>Lägg till kurs-pm information som PDF</CardText>
+                <CardTitle>{i18n.messages[lang].startCards.coursePM_hd}</CardTitle>
+                <CardText>{i18n.messages[lang].startCards.coursePM_desc}</CardText>
                 {/* <CardText><TextBlock text={this.state.sellingText} /></CardText> */}
               </CardBody>
-              <CardFooter className='text-right'><Button onClick={this.doStartCoursePM} color='primary'>Ladda upp kurs-pm</Button></CardFooter>
+              <CardFooter className='text-right'>
+                <Link to='#' className='btn btn-primary'>{i18n.messages[lang].startCards.coursePM_btn}</Link>
+              </CardFooter>
             </Card>
             <Card>
               <CardBody>
-                <CardTitle>Kursutveckling</CardTitle>
-                <CardText>Lägg till kurssäljande information för att tydligare förklare varför studenter behöver den kursen</CardText>
+                <CardTitle>{i18n.messages[lang].startCards.courseDev_hd}</CardTitle>
+                <CardText>{i18n.messages[lang].startCards.courseDev_decs}</CardText>
                 {/* <CardText><TextBlock text={this.state.sellingText} /></CardText> */}
               </CardBody>
-              <CardFooter className='text-right'><Button onClick={this.doStartCourseDev} color='primary'>Lägg till kursutveckling</Button></CardFooter>
+              <CardFooter className='text-right'>
+                <Link to='#' className='btn btn-primary'>{i18n.messages[lang].startCards.courseDev_btn}</Link>
+                {/* <Button onClick={this.doStartCourseDev} color='primary'>{i18n.messages[lang].startCards.courseDev_btn}</Button> */}
+              </CardFooter>
             </Card>
-          </div>
-        )}
+          </span>
+        </div>
+        <span className='Header--Button'>
+          <a href={`/admin/kurser/kurs/${courseCode}/my`} class='link-back'>Översikt av mina kurser</a>
+        </span>
       </div>
     )
   }
