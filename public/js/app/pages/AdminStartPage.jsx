794443ef55a0f2a5774179e0b24a0bc7e19d9434
Elena Rakhimova <elenara@kth.se>
2019-03-04 18:34:36 +0100

    Refactore translations for start page

--- public/js/app/pages/AdminStartPage.jsx
+++ public/js/app/pages/AdminStartPage.jsx
@@ -18,55 +18,56 @@ class AdminStartPage extends Component {
     const courseAdminData = adminStore['courseAdminData']
     const lang = courseAdminData.lang === 'en' ? 0 : 1
     const courseCode = courseAdminData.courseTitleData.course_code
+    const translation = i18n.messages[lang]
 
     return (
       <div key='kursinfo-container' className='kursinfo-main-page col' >
         {/* ---COURSE TITEL--- */}
         <CourseTitle key='title'
           courseTitleData={courseAdminData.courseTitleData}
-          pageTitle={i18n.messages[lang].pageTitles.administrate}
+          pageTitle={translation.pageTitles.administrate}
           language={courseAdminData.lang}
         />
         {this.props.location.data === 'success' ?
           <Alert color='success' aria-live='polite'>
-            {i18n.messages[lang].alertMessages.success}
+            {translation.alertMessages.success}
           </Alert>
         : ''
         }
         <div className='col'>
           <span className='Header--Link'>
-            <a href={`/student/kurser/kurs/${courseCode}?l=${courseAdminData.lang}`} alt={i18n.messages[lang].altLabel.start_link_back} class='link-back'>{i18n.messages[lang].sellingTextButtons.button_course_info}</a>
+            <a href={`/student/kurser/kurs/${courseCode}?l=${courseAdminData.lang}`} alt={translation.altLabel.start_link_back} class='link-back'>{translation.sellingTextButtons.button_course_info}</a>
           </span>
           <span className='AdminPage--ShowDescription'>
             <Card className='KursInfo--SellingText'>
               <CardBody>
-                <CardTitle>{i18n.messages[lang].startCards.sellingText_hd}</CardTitle>
-                <CardText>{i18n.messages[lang].startCards.sellingText_desc}</CardText>
+                <CardTitle>{translation.startCards.sellingText_hd}</CardTitle>
+                <CardText>{translation.startCards.sellingText_desc}</CardText>
               </CardBody>
               <CardFooter className='text-right'>
-                <a href={`/admin/kurser/kurs/edit/${courseCode}?l=${courseAdminData.lang}`} alt={i18n.messages[lang].altLabel.sellingText_btn} className='btn btn-primary'>{i18n.messages[lang].startCards.sellingText_btn}</a>
+                <a href={`/admin/kurser/kurs/edit/${courseCode}?l=${courseAdminData.lang}`} alt={translation.altLabel.sellingText_btn} className='btn btn-primary'>{translation.startCards.sellingText_btn}</a>
                 {/* <Link to={{ pathname: `/admin/kurser/kurs/edit/${courseCode}?l=${courseAdminData.lang}`,
                 data: 'hello'
-                }} className='btn btn-primary' onClick={this.doStartSellingText}>{i18n.messages[lang].startCards.sellingText_btn}</Link> */}
-                {/* <Button onClick={this.doStartSellingText} color='primary'>{i18n.messages[lang].startCards.sellingText_btn}</Button> */}
+                }} className='btn btn-primary' onClick={this.doStartSellingText}>{translation.startCards.sellingText_btn}</Link> */}
+                {/* <Button onClick={this.doStartSellingText} color='primary'>{translation.startCards.sellingText_btn}</Button> */}
               </CardFooter>
             </Card>
             <Card>
               <CardBody>
-                <CardTitle>{i18n.messages[lang].startCards.coursePM_hd}</CardTitle>
-                <CardText>{i18n.messages[lang].startCards.coursePM_desc}</CardText>
+                <CardTitle>{translation.startCards.coursePM_hd}</CardTitle>
+                <CardText>{translation.startCards.coursePM_desc}</CardText>
               </CardBody>
               <CardFooter className='text-right'>
-                <Link to='#' className='btn btn-primary' alt={i18n.messages[lang].altLabel.coursePM_btn}>{i18n.messages[lang].startCards.coursePM_btn}</Link>
+                <Link to='#' className='btn btn-primary' alt={translation.altLabel.coursePM_btn}>{translation.startCards.coursePM_btn}</Link>
               </CardFooter>
             </Card>
             <Card>
               <CardBody>
-                <CardTitle>{i18n.messages[lang].startCards.courseDev_hd}</CardTitle>
-                <CardText>{i18n.messages[lang].startCards.courseDev_decs}</CardText>
+                <CardTitle>{translation.startCards.courseDev_hd}</CardTitle>
+                <CardText>{translation.startCards.courseDev_decs}</CardText>
               </CardBody>
               <CardFooter className='text-right'>
-                <Link to='#' className='btn btn-primary' alt={i18n.messages[lang].altLabel.courseDev_btn}>{i18n.messages[lang].startCards.courseDev_btn}</Link>
+                <Link to='#' className='btn btn-primary' alt={translation.altLabel.courseDev_btn}>{translation.startCards.courseDev_btn}</Link>
               </CardFooter>
             </Card>
           </span>
