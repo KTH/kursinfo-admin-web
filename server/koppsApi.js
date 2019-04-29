56eeac05a6bdb2955ca54f784a14ab35d0412e10
Elena Rakhimova <elenara@kth.se>
2019-04-29 12:22:03 +0200

    Added separate kopps api handling and using cache

--- server/koppsApi.js
+++ server/koppsApi.js
@@ -1,22 +1,23 @@
 const { BasicAPI } = require('kth-node-api-call')
-const serverConfig = require('./configuration').server
+const { server } = require('./configuration')
+const log = require('kth-node-log')
 
 const koppsApi = new BasicAPI({
-  hostname: serverConfig.kopps.host,
-  basePath: serverConfig.kopps.basePath,
-  https: serverConfig.kopps.https,
+  hostname: server.kopps.host,
+  basePath: server.kopps.basePath,
+  https: server.kopps.https,
   json: true,
-  defaultTimeout: serverConfig.kopps.defaultTimeout
+  defaultTimeout: server.kopps.defaultTimeout
 })
 
 const koppsCourseData = async (courseCode) => {
   try {
-    const course = await koppsApi.getAsync({ uri: `course/${courseCode}`, useCache: true })
-    return course.data
+    const course = await koppsApi.getAsync({ uri: `course/${encodeURIComponent(courseCode)}`, useCache: true })
+    console.log('coursekokdsfadssdsd', course.body)
+    return course.body
   } catch (err) {
-    if (err.response) {
-    }
-    return {}
+    log.error('Exception calling from koppsAPI in koppsApi.koppsCourseData', { error: err })
+    throw err
   }
 }
 
