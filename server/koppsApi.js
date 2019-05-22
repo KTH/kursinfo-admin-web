470fcc1361a3fc019853813387cd05b9255e5add
Elena Rakhimova <elenara@kth.se>
2019-05-22 10:35:15 +0200

    Added redis to kopps api

--- server/koppsApi.js
+++ server/koppsApi.js
@@ -1,13 +1,19 @@
 const { BasicAPI } = require('kth-node-api-call')
 const { server } = require('./configuration')
 const log = require('kth-node-log')
+const redis = require('kth-node-redis')
 
 const koppsApi = new BasicAPI({
   hostname: server.kopps.host,
   basePath: server.kopps.basePath,
   https: server.kopps.https,
   json: true,
-  defaultTimeout: server.kopps.defaultTimeout
+  defaultTimeout: server.kopps.defaultTimeout,
+  redis: {
+    client: redis,
+    prefix: 'course-info-admin-kopps',
+    expire: 20000
+  }
 })
 
 const koppsCourseData = async (courseCode) => {
