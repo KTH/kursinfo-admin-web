29d39772bf20227d8c6c62c2c4463d7cc7ea25db
Elena Rakhimova <elenara@kth.se>
2019-05-28 17:22:25 +0200

    Returned redis cache for kursinfoapi

--- config/serverSettings.js
+++ config/serverSettings.js
@@ -99,6 +99,10 @@ module.exports = {
   cache: {
     cortinaBlock: {
       redis: unpackRedisConfig('REDIS_URI', devRedis)
+    },
+    kursinfoApi: {// TODO, CHECK IF IT IS NEEDED
+      redis: unpackRedisConfig('REDIS_URI', devRedis),
+      expireTime: getEnv('KURSINFO_API_CACHE_EXPIRE_TIME', 60 * 60 * 1000)
     }
   },
 
