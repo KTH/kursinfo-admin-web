007566af66fb8e3373480189e0bd74a8a8da40f9
Elena Rakhimova <elenara@kth.se>
2019-05-28 16:29:11 +0200

    Removed old stuff

--- server/controllers/sellingInfoCtrl.js
+++ server/controllers/sellingInfoCtrl.js
@@ -70,7 +70,7 @@ async function _getDescription (req, res, next) {
     })
     const html = renderToString(renderProps)
     res.render('course/index', {
-      debug: 'debug' in req.query,
+      // debug: 'debug' in req.query,
       instrumentationKey: serverConfig.appInsights.instrumentationKey,
       title: courseCode,
       html: html,
