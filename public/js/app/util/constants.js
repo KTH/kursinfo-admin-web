775f19fe44b90ca162d7f26a1928490d01907de4
Elena Rakhimova <elenara@kth.se>
2019-06-18 15:20:16 +0200

    Added const for kth se link for prod

--- public/js/app/util/constants.js
+++ public/js/app/util/constants.js
@@ -1,5 +1,6 @@
 export const PROGRAMME_URL = 'https://www.kth.se/student/kurser/program'
 export const EMPTY = '--no information--'
+export const KTH_SE_URL = '' // TODO www.kth.se
 export const FORSKARUTB_URL = 'https://www.kth.se/utbildning/forskarutbildning/kurser/org'
 export const COURSE_INFO_URL = '/student/kurser/kurs/'
 export const COURSE_UTVECKLING = '/kursutveckling/'
