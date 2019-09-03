9b2bcedb6d2304f23354df26ce834e9fda230dbd
Elena Rakhimova <elenara@kth.se>
2019-09-03 12:59:47 +0200

    Added extra temp route for a picture

--- server/server.js
+++ server/server.js
@@ -182,6 +182,16 @@ server.use(excludeExpression, require('kth-node-web-common/lib/web/crawlerRedire
   hostUrl: config.hostUrl
 }))
 
+/* ********************************
+ * ******* FILE UPLOAD*******
+ * ********************************
+ */
+
+const fileUpload = require('express-fileupload')
+server.use(fileUpload())
+server.use(bodyParser.json({ limit: '50mb' }))
+server.use(bodyParser.urlencoded({ limit: '50mb', extended: true }))
+
 /* **********************************
  * ******* APPLICATION ROUTES *******
  * **********************************
@@ -206,7 +216,7 @@ appRoute.get('course.editDescription', config.proxyPrefixPath.uri + '/picture/:c
 appRoute.post('course.updateDescription', config.proxyPrefixPath.uri + '/api/:courseCode/', serverLogin, requireRole('isCourseResponsible', 'isExaminator', 'isSuperUser'), SellingInfo.updateDescription)
 appRoute.get('api.koppsCourseData', config.proxyPrefixPath.uri + '/getKoppsCourseDataByCourse/:courseCode/', AdminPagesCtrl.getKoppsCourseData)
 // File upload for a course picture
-appRoute.post('storage.saveFile', config.proxyPrefixPath.uri + '/storage/saveFile/:pictureid', SellingInfo.saveFileToStorage)
+appRoute.post('storage.saveFile', config.proxyPrefixPath.uri + '/storage/saveFile/:pictureid/', SellingInfo.saveFileToStorage)
 // appRoute.post('storage.updateFile', config.proxyPrefixPath.uri + '/storage/updateFile/:fileName/', SellingInfo.updateFileInStorage)
 // appRoute.post('storage.deleteFile', config.proxyPrefixPath.uri + '/storage/deleteFile/:id', SellingInfo.deleteFileInStorage)
 appRoute.get('system.gateway', config.proxyPrefixPath.uri + '/gateway', getServerGatewayLogin('/'), requireRole('isCourseResponsible', 'isExaminator', 'isSuperUser'), SellingInfo.getDescription)
