8008ee1c8676f9a8fff29526683965d9c99ab00d
Elena Rakhimova <elenara@kth.se>
2019-09-05 16:24:10 +0200

    Rebuild main page and fixed modal and function for changing its states from children

--- public/js/app/pages/CourseDescriptionEditorPage.jsx
+++ public/js/app/pages/CourseDescriptionEditorPage.jsx
@@ -5,7 +5,6 @@ import i18n from '../../../../i18n'
 import CourseTitle from '../components/CourseTitle.jsx'
 import { KURSINFO_IMAGE_BLOB_URL } from '../util/constants'
 import ProgressBar from '../components/ProgressBar.jsx'
-
 import PictureUpload from './PictureUpload.jsx'
 import SellingInfo from './SellingInfo.jsx'
 
@@ -15,13 +14,16 @@ class CourseDescriptionEditorPage extends Component {
     super(props)
     this.state = {
       enteredUploadMode: this.props.adminStore.enteredUploadMode,
-      progress: 1
+      progress: 1,
+      isNewFile: false, // ????
+      file: undefined
     }
     this.courseAdminData = this.props.adminStore.courseAdminData
     this.courseCode = this.courseAdminData.courseTitleData.course_code
     this.userLang = this.courseAdminData.lang
     this.langIndex = this.courseAdminData.lang === 'en' ? 0 : 1
-    this.doNextStep = this.doNextStep.bind(this)
+    // this.doNextStep = this.doNextStep.bind(this)
+    this.doUpdateStates = this.doUpdateStates.bind(this)
   }
   componentDidMount () {
     console.log('componentDidMount')
@@ -37,41 +39,33 @@ class CourseDescriptionEditorPage extends Component {
 
   componentWillReceiveProps (nextProps) {
     console.log('componentWillReceiveProps')
-    // if (nextProps.scrollTop !== 0) {
-    //   this.setState({scrollTop: nextProps.scrollTop})
-    // }
   }
-  doNextStep (isNewFile, tempFile, progress) {
-    // event.preventDefault()
-    console.log('efter next step event', isNewFile, tempFile)
-    this.setState({
-      isNewFile,
-      tempFile,
-      enteredUploadMode: false,
-      progress
-    })
+
+  doUpdateStates (states) {
+    console.log('WOHO', states)
+    if (states) this.setState(states)
   }
-  render ({adminStore}) {
+
+  render () {
     const { courseAdminData, userLang, langIndex } = this
-    const { courseImage, pageTitles, sellingTextLabels } = i18n.messages[langIndex]
+    const { courseImage, introLabel } = i18n.messages[langIndex]
     let courseImageID = courseImage[courseAdminData.imageFileName]
     if (courseImageID === undefined) courseImageID = courseImage.default
     const imageUrl = `${KURSINFO_IMAGE_BLOB_URL}${courseImageID}`
     return (
       <div key='kursinfo-container' className='kursinfo-main-page col'>
-        {/* ---COURSE TITEL--- */}
         <CourseTitle key='title'
           courseTitleData={courseAdminData.courseTitleData}
-          pageTitle={pageTitles.edit_course_introduction}
+          pageTitle={introLabel.editCourseIntro}
           language={userLang}
           />
         <ProgressBar active={this.state.progress} language={langIndex} />
         {this.state.enteredUploadMode
-            ? <PictureUpload imageUrl={imageUrl} sellingTextLabels={sellingTextLabels}
-              courseAdminData={courseAdminData}
-              nextStep={this.doNextStep} />
-            : <SellingInfo courseAdminData={courseAdminData} />
-          }
+        ? <PictureUpload imageUrl={imageUrl} introLabel={introLabel}
+          courseAdminData={courseAdminData}
+          updateParent={this.doUpdateStates} />
+        : <SellingInfo courseAdminData={courseAdminData} updateParent={this.doUpdateStates} />
+        }
       </div>
     )
   }
