257786004d7aa57a245bef0eec1c2d386d63fc43
Elena Rakhimova <elenara@kth.se>
2019-09-06 16:07:45 +0200

    removed preview mode from editor mode and cleaned up variables

--- public/js/app/pages/SellingInfo.jsx
+++ public/js/app/pages/SellingInfo.jsx
@@ -31,24 +31,21 @@ class SellingInfo extends Component {
   constructor (props) {
     super(props)
     this.state = {
-      sv: this.props.adminStore.sellingText.sv,
+      sv: this.props.adminStore.sellingText.sv,
       en: this.props.adminStore.sellingText.en,
       leftTextSign_sv: undefined,
       leftTextSign_en: undefined,
-      enteredEditMode: true,
       hasDoneSubmit: false,
       isError: false,
       errMsg: ''
     }
-    this.sellingTextAuthor = this.props.adminStore.sellingTextAuthor,
-    this.courseAdminData = this.props.adminStore.courseAdminData
-    this.courseCode = this.courseAdminData.courseTitleData.course_code
-    this.userLang = this.courseAdminData.lang
-    this.langIndex = this.courseAdminData.lang === 'en' ? 0 : 1
-    this.goTo = this.goTo.bind(this)
-    this.goToEditMode = this.goToEditMode.bind(this)
-    this.handlePublish = this.handlePublish.bind(this)
+    this.sellingTextAuthor = this.props.adminStore.sellingTextAuthor
+    this.koppsData = this.props.adminStore.koppsData
+    this.courseCode = this.koppsData.courseTitleData.course_code
+    this.userLang = this.koppsData.lang
+    this.langIndex = this.koppsData.lang === 'en' ? 0 : 1
     this.startEditor = this.startEditor.bind(this)
+    this.quiteEditor = this.quiteEditor.bind(this)
   }
 
   componentDidMount () {
@@ -60,73 +57,10 @@ class SellingInfo extends Component {
   }
 
   componentDidUpdate () {
-    console.log('sell componentDidUpdate')
     window.addEventListener('load', this.startEditor)
   }
 
-  goToEditMode (event) {
-    event.preventDefault()
-    this.setState({
-      hasDoneSubmit: false,
-      enteredEditMode: true,
-      isError: false
-    })
-    this.startEditor(event)
-    const states = {
-      // enteredUploadMode: false,
-      progress: 2
-    }
-    this.props.updateParent(states)
-  }
-
-  handlePublish () {
-    // event.preventDefault()
-    const {courseCode, langIndex, lang} = this
-    const sellingTexts = this._shapeText()
-    this.props.uploadFinalPic()
-    this.props.adminStore.doUpsertItem(sellingTexts, courseCode).then(() => {
-      this.setState({
-        hasDoneSubmit: true,
-        isError: false
-      })
-      window.location = `${ADMIN_OM_COURSE}${courseCode}?l=${lang}&serv=kinfo&event=pub`
-    }).catch(err => {
-      this.setState({
-        hasDoneSubmit: false,
-        isError: true,
-        errMsg: i18n.messages[langIndex].pageTitles.alertMessages.api_error
-      })
-    })
-  }
-
-  _shapeText () {
-    return {
-      sv: this.state.sv,
-      en: this.state.en
-    }
-  }
-
-  goTo (event) {
-    event.preventDefault()
-    const sellingTexts = this._shapeText()
-    const progress = event.target.id === 'back-to-image' ? 1 : 3
-    console.log('this.sellingText in1', sellingTexts)
-    console.log('en 1', this.state.en)
-    this.props.adminStore.updateSellingText(sellingTexts)
-    this.setState({
-      enteredEditMode: false,
-      isError: false
-    })
-    CKEDITOR.instances.sv.destroy(true)
-    CKEDITOR.instances.en.destroy(true)
-    const states = {
-      progress
-    }
-    this.props.updateParent(states)
-  }
-
-
-  _doCalculateLength = (event, editorId) => {
+  _countTextLen (event, editorId) {
     const text = event.editor.document.getBody().getText().replace(/\n/g, '')
     const length = text.length
     this.setState({
@@ -137,9 +71,9 @@ class SellingInfo extends Component {
     return [text, length]
   }
 
-  _doCheckTextLength = (event, l) => {
+  _validateLen (event, l) {
     const translation = i18n.messages[this.langIndex].pageTitles.alertMessages
-    const [cleanText, cleanTextLen] = this._doCalculateLength(event, l)
+    const [cleanText, cleanTextLen] = this._countTextLen(event, l)
     const htmlText = event.editor.getData()
     if (htmlText.length > 10000) { // this is max in api
       this.setState({
@@ -162,19 +96,37 @@ class SellingInfo extends Component {
     }
   }
 
+  _shapeText () {
+    return {
+      sv: this.state.sv,
+      en: this.state.en
+    }
+  }
+
   startEditor () {
     ['sv', 'en'].map((editorId) => {
       let textArea = document.getElementById(editorId)
       CKEDITOR.replace(editorId, editorConf)
-      CKEDITOR.instances[editorId].on('instanceReady', event => this._doCalculateLength(event, editorId))
-      CKEDITOR.instances[editorId].on('change', event => this._doCheckTextLength(event, editorId))
+      CKEDITOR.instances[editorId].on('instanceReady', event => this._countTextLen(event, editorId))
+      CKEDITOR.instances[editorId].on('change', event => this._validateLen(event, editorId))
     })
   }
 
+  quiteEditor (event) {
+    event.preventDefault()
+    const sellingTexts = this._shapeText()
+    const progress = event.target.id === 'back-to-image' ? 1 : 3
+    this.props.adminStore.updateSellingText(sellingTexts)
+    this.setState({ isError: false })
+    CKEDITOR.instances.sv.destroy(true)
+    CKEDITOR.instances.en.destroy(true)
+    this.props.updateParent({progress})
+  }
+
   render ({adminStore}) {
-    const { courseAdminData, langIndex } = this
+    const { koppsData, langIndex } = this
     const { courseImage, introLabel } = i18n.messages[langIndex]
-    let courseImageID = courseImage[courseAdminData.imageFileName]
+    let courseImageID = courseImage[koppsData.imageFileName]
     if (courseImageID === undefined) courseImageID = courseImage.default
     const imageUrl = `${KURSINFO_IMAGE_BLOB_URL}${courseImageID}`
     return (
@@ -183,7 +135,7 @@ class SellingInfo extends Component {
         {this.state.errMsg ? <Alert color='info'><p>{this.state.errMsg}</p></Alert> : ''}
 
         {/* ---IF in edit mode or preview mode--- */}
-        {this.state.enteredEditMode ? (
+        {/* {this.state.enteredEditMode ? ( */}
           <div className='TextEditor--SellingInfo col'>
             {/* ---TEXT Editors for each language--- */}
             <p>{introLabel.step_2_desc}</p>
@@ -191,13 +143,13 @@ class SellingInfo extends Component {
             <span className='Editors--Area' key='editorsArea' role='tablist'>
               <span className='left' key='leftEditorForSwedish'>
                 <KoppsTextCollapse instructions={introLabel}
-                  koppsText={courseAdminData.koppsCourseDesc['sv']} lang='sv' />
+                  koppsText={koppsData.koppsText['sv']} lang='sv' />
                 <p>{introLabel.label_left_number_letters}<span className='badge badge-warning badge-pill'>{this.state.leftTextSign_sv}</span></p>
                 <textarea name='sv' id='sv' className='editor' style='visibility: hidden; display: none;'>{this.state.sv}</textarea>
               </span>
               <span className='right' key='rightEditorForEnglish'>
                 <KoppsTextCollapse instructions={introLabel}
-                  koppsText={courseAdminData.koppsCourseDesc['en']} lang='en' />
+                  koppsText={koppsData.koppsText['en']} lang='en' />
                 <p>{introLabel.label_left_number_letters}<span className='badge badge-warning badge-pill'>{this.state.leftTextSign_en}</span></p>
                 <textarea name='en' id='en' className='editor' style='visibility: hidden; display: none;'>{this.state.en}</textarea>
               </span>
@@ -205,7 +157,7 @@ class SellingInfo extends Component {
             <p className='changed-by'>{introLabel.changed_by} {this.sellingTextAuthor}</p>
             <Row className='control-buttons'>
               <Col sm='4' className='btn-back'>
-                <Button onClick={this.goTo} id='back-to-image' alt={introLabel.alt.step1}>
+                <Button onClick={this.quiteEditor} id='back-to-image' alt={introLabel.alt.step1}>
                   {introLabel.button.step1}
                 </Button>
               </Col>
@@ -213,43 +165,12 @@ class SellingInfo extends Component {
                 <ButtonModal id='cancel' step={2} course={this.courseCode} buttonLabel={introLabel.button.cancel} infoText={introLabel.info_cancel} />
               </Col>
               <Col sm='4' className='btn-next'>
-                <Button onClick={this.goTo} id='to-peview' color='success' alt={introLabel.alt.step3} disabled={this.state.isError}>
+                <Button onClick={this.quiteEditor} id='to-peview' color='success' alt={introLabel.alt.step3} disabled={this.state.isError}>
                   {introLabel.button.step3}
                 </Button>
               </Col>
             </Row>
           </div>
-        ) : (
-          <div className='col'>
-            <h2>{introLabel.label_step_3}</h2>
-            <Row id='pageContainer' key='pageContainer'>
-              <Col sm='12' xs='12' lg='12' id='middle' key='middle'>
-                <PreviewText introLabel={introLabel} whichLang='sv'
-                  image={imageUrl}
-                  sellingText={this.state.sv} koppsTexts={courseAdminData.koppsCourseDesc} />
-                <PreviewText introLabel={introLabel} whichLang='en'
-                  image={imageUrl}
-                  sellingText={this.state.en} koppsTexts={courseAdminData.koppsCourseDesc} />
-                <Row className='control-buttons'>
-                  <Col sm='4' className='btn-back'>
-                    <Button onClick={this.goToEditMode} alt={introLabel.alt.step2Back}>{introLabel.button.step2}</Button>
-                  </Col>
-                  <Col sm='4' className='btn-cancel'>
-                    <ButtonModal id='cancel' step={3} course={this.courseCode} buttonLabel={introLabel.button.cancel} infoText={introLabel.info_cancel} />
-                  </Col>
-                  <Col sm='4' className='btn-last'>
-                    <ButtonModal id='publish' buttonLabel={introLabel.button.publish}
-                      handleConfirm={this.handlePublish} infoText={introLabel.info_publish} alt={introLabel.alt.publish} />
-{/*
-                    <Button id='publish' key='publish' onClick={this.toggleModal} color='success' alt={introLabel.alt.publish}>
-                      {introLabel.button.publish}
-                    </Button> */}
-                  </Col>
-                </Row>
-              </Col>
-            </Row>
-          </div>
-        )}
       </div>
     )
   }
