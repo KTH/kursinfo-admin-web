884e38b0deeaa1ebb345799a67c8554b5ae87bb2
Elena Rakhimova <elenara@kth.se>
2019-09-06 14:41:32 +0200

    removed boolean value and changed to progress step 1-3

--- public/js/app/pages/CourseDescriptionEditorPage.jsx
+++ public/js/app/pages/CourseDescriptionEditorPage.jsx
@@ -7,16 +7,27 @@ import { KURSINFO_IMAGE_BLOB_URL } from '../util/constants'
 import ProgressBar from '../components/ProgressBar.jsx'
 import PictureUpload from './PictureUpload.jsx'
 import SellingInfo from './SellingInfo.jsx'
+import Progress from 'inferno-bootstrap/dist/Progress'
+
+const _getTodayDate = (date = '') => {
+  let today = date.length > 0 ? new Date(date) : new Date()
+  let dd = String(today.getDate()).padStart(2, '0')
+  let mm = String(today.getMonth() + 1).padStart(2, '0') // January is 0!
+  let yyyy = today.getFullYear()
+  return yyyy + '-' + mm + '-' + dd
+}
 
 @inject(['adminStore']) @observer
 class CourseDescriptionEditorPage extends Component {
   constructor (props) {
     super(props)
     this.state = {
-      enteredUploadMode: this.props.adminStore.enteredUploadMode,
+      // enteredUploadMode: this.props.adminStore.enteredUploadMode,
       progress: 1,
       isNewFile: false, // ????
-      file: undefined
+      imageFile: undefined,
+      fileProgress: 0, // ???
+      isPublished: 'published' // false
     }
     this.courseAdminData = this.props.adminStore.courseAdminData
     this.courseCode = this.courseAdminData.courseTitleData.course_code
@@ -24,6 +35,7 @@ class CourseDescriptionEditorPage extends Component {
     this.langIndex = this.courseAdminData.lang === 'en' ? 0 : 1
     // this.doNextStep = this.doNextStep.bind(this)
     this.doUpdateStates = this.doUpdateStates.bind(this)
+    this.handleUploadImage = this.handleUploadImage.bind(this)
   }
   componentDidMount () {
     console.log('componentDidMount')
@@ -41,9 +53,51 @@ class CourseDescriptionEditorPage extends Component {
     console.log('componentWillReceiveProps')
   }
 
+  _handleAction () {
+    console.log('MAYBE UPLOAD FILE')
+  }
+
   doUpdateStates (states) {
     console.log('WOHO', states)
     if (states) this.setState(states)
+    this._handleAction()
+  }
+
+  handleUploadImage (/** */) {
+    const formData = this.state.imageFile
+    const thisInstance = this
+    let fileProgress = this.state.fileProgress
+    return new Promise((resolve, reject) => {
+      const req = new XMLHttpRequest()
+      req.upload.addEventListener('progress', event => {
+        if (event.lengthComputable) {
+          fileProgress = (event.loaded / event.total) * 100
+          console.log(fileProgress)
+          this.setState({ fileProgress: fileProgress })
+        }
+      })
+
+      req.onreadystatechange = function () {
+        // console.log('onreadystatechange values', values)
+
+        if (this.readyState === 4 && this.status === 200) {
+          if (formData) {
+            thisInstance.state.fileSavedDate = _getTodayDate()
+            thisInstance.setState({
+              isError: false, // todo: remove
+              successMsg: 'Success', // i18n.messages[thisInstance.props.routerStore.language].messages.alert_uploaded_file,
+              errMsg: undefined,
+              hasNewUploadedImage: true
+            })
+            console.log('Ura 1', thisInstance.state)
+          }
+          console.log('Ura 2')
+        }
+      }
+      req.open('POST',
+        `${this.props.adminStore.browserConfig.hostUrl}${this.props.adminStore.paths.storage.saveFile.uri.split(':')[0]}${this.courseCode}/${this.state.isPublished}`)
+      req.send(formData)
+    })
   }
 
   render () {
@@ -60,12 +114,18 @@ class CourseDescriptionEditorPage extends Component {
           language={userLang}
           />
         <ProgressBar active={this.state.progress} language={langIndex} />
-        {this.state.enteredUploadMode
+        {this.state.progress === 1
         ? <PictureUpload imageUrl={imageUrl} introLabel={introLabel}
           courseAdminData={courseAdminData}
           updateParent={this.doUpdateStates} />
-        : <SellingInfo courseAdminData={courseAdminData} updateParent={this.doUpdateStates} />
+        : <SellingInfo courseAdminData={courseAdminData} updateParent={this.doUpdateStates}
+          uploadFinalPic={this.handleUploadImage}
+          />
         }
+        <span>
+          <div className='text-center'>{this.state.fileProgress}%</div>
+          <Progress value={this.state.fileProgress} />
+        </span>
       </div>
     )
   }
