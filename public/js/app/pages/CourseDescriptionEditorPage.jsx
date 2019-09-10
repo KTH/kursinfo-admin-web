dbf527ba773c73a2387465f1968ca400ef7fc570
Elena Rakhimova <elenara@kth.se>
2019-09-10 16:21:05 +0200

    removed empty functions and replaced storageuri

--- public/js/app/pages/CourseDescriptionEditorPage.jsx
+++ public/js/app/pages/CourseDescriptionEditorPage.jsx
@@ -3,19 +3,11 @@ import { inject, observer } from 'inferno-mobx'
 import i18n from '../../../../i18n'
 
 import CourseTitle from '../components/CourseTitle.jsx'
-import { KURSINFO_IMAGE_BLOB_URL } from '../util/constants'
 import ProgressBar from '../components/ProgressBar.jsx'
 import PictureUpload from './PictureUpload.jsx'
 import SellingInfo from './SellingInfo.jsx'
 import Preview from '../components/PreviewText.jsx'
 
-const _getTodayDate = (date = '') => {
-  let today = date.length > 0 ? new Date(date) : new Date()
-  let dd = String(today.getDate()).padStart(2, '0')
-  let mm = String(today.getMonth() + 1).padStart(2, '0') // January is 0!
-  let yyyy = today.getFullYear()
-  return yyyy + '-' + mm + '-' + dd
-}
 
 @inject(['adminStore']) @observer
 class CourseDescriptionEditorPage extends Component {
@@ -26,34 +18,14 @@ class CourseDescriptionEditorPage extends Component {
     }
     this.koppsData = this.props.adminStore.koppsData
     this.courseCode = this.koppsData.courseTitleData.course_code
+    this.storageUri = this.props.adminStore.browserConfig.storageUri
     this.userLang = this.koppsData.lang
     this.langIndex = this.koppsData.lang === 'en' ? 0 : 1
     this.doUpdateStates = this.doUpdateStates.bind(this)
   }
-  componentDidMount () {
-    console.log('componentDidMount')
-  }
-
-  componentWillUnmount () {
-    console.log('componentDidMount')
-  }
-
-  componentDidUpdate () {
-    console.log('componentDidUpdate')
-  }
-
-  componentWillReceiveProps (nextProps) {
-    console.log('componentWillReceiveProps')
-  }
-
-  _handleAction () {
-    console.log('MAYBE UPLOAD FILE')
-  }
 
   doUpdateStates (states) {
-    console.log('WOHO', states)
     if (states) this.setState(states)
-    this._handleAction()
   }
 
   render () {
@@ -61,7 +33,7 @@ class CourseDescriptionEditorPage extends Component {
     const { courseImage, introLabel } = i18n.messages[langIndex]
     let courseImageID = courseImage[koppsData.defaultPicName]
     if (courseImageID === undefined) courseImageID = courseImage.default
-    const defaultImageUrl = `${KURSINFO_IMAGE_BLOB_URL}${courseImageID}`
+    const defaultImageUrl = `${this.storageUri}${courseImageID}`
     return (
       <div key='kursinfo-container' className='kursinfo-main-page col'>
         <CourseTitle key='title'
