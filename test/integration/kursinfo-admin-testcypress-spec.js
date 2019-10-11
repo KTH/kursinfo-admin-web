a76d190eb2e146ccc87ef9868a76e51bdcf6074d
Murk Gopi Chand <murk.gopichand@r2m.se>
2019-10-11 13:11:12 +0200

    An example with cypress tests

---
+++ test/integration/kursinfo-admin-testcypress-spec.js
@@ -0,0 +1,90 @@
+
+describe('My First Test', function() {
+    it('Does not do much!', function() {
+        expect(true).to.equal(true)
+    })
+})
+
+
+describe('Login to kursinfo-admin-web', function () {
+    context('form submission', function () {
+        beforeEach(function () {
+            cy.visit('https://login-r.referens.sys.kth.se')
+        })
+
+        it('displays errors on login', function () {
+            // incorrect username on purpose
+            cy.get('input[name=username]').type('jane.lae')
+            cy.get('input[name=password]').type('password123{enter}')
+
+            // we should have visible errors now
+            cy.contains('Försök igen')
+
+            // and still be on the same URL
+            cy.url().should('include', '/login')
+        })
+    })
+
+})
+
+
+describe('Can access admin page', function () {
+    const username = Cypress.env('username')
+    const password = Cypress.env('password')
+
+    context('direct navigate to admin page', function () {
+        beforeEach(function () {
+            cy.visit('http://localhost:3001/kursinfoadmin/kurser/kurs/edit/SF1624')
+        })
+
+        it('asks for login', function () {
+            cy.get('input[name=username]').type(username)
+            cy.get('input[name=password]').type(password+'{enter}')
+
+            cy.get('#course-title h1').should('be.visible')
+        })
+    })
+
+
+    context('Reusable "login" custom command', function () {
+        // typically we'd put this in cypress/support/commands.js
+        // but because this custom command is specific to this example
+        // we'll keep it here
+        Cypress.Commands.add('loginByForm', (username, password) => {
+            Cypress.log({
+                name: 'loginByForm',
+                message: `${username} | ${password}`,
+            })
+
+            return cy.request({
+                method: 'POST',
+                url: 'https://login-r.referens.sys.kth.se',
+                form: true,
+                body: {
+                    username,
+                    password,
+                },
+            })
+        })
+
+        beforeEach(function () {
+            // login before each test
+            cy.loginByForm(username, password)
+        })
+
+        it('can visit /dashboard', function () {
+            // after cy.request, the session cookie has been set
+            // and we can visit a protected page
+            cy.visit('http://localhost:3001/kursinfoadmin/kurser/kurs/edit/SF1624')
+            cy.get('#course-title h1').should('be.visible')
+            cy.get("#course-title h1").should('have.text', 'Redigera introduktion till kursen')
+        })
+
+    })
+
+
+
+
+
+
+})
