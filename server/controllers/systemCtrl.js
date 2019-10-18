09d65f17dada43d2470889fd84c0a122f201bdd1
Rickard Falk <rickardfalk@n131-p162.eduroam.kth.se>
2019-10-18 12:41:25 +0200

    Log 404 in debug instead of error

--- server/controllers/systemCtrl.js
+++ server/controllers/systemCtrl.js
@@ -44,9 +44,14 @@ function _notFound (req, res, next) {
 
 // this function must keep this signature for it to work properly
 function _final (err, req, res, next) {
-  log.error({ err: err }, 'Unhandled error')
-
   const statusCode = err.status || err.statusCode || 500
+
+  if (statusCode === 404) {
+    log.debug({ err: err })
+  } else {
+    log.error({ err: err }, 'Unhandled error')
+  }
+
   const isProd = (/prod/gi).test(process.env.NODE_ENV)
   const lang = language.getLanguage(res)
 
