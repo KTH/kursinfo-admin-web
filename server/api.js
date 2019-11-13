b3c207f690539eec792dd65a00532c6e1920bc35
Elena Rakhimova <elenara@kth.se>
2019-11-13 14:12:13 +0100

    added a cache for kursinfo-api

--- server/api.js
+++ server/api.js
@@ -6,8 +6,10 @@ const redis = require('kth-node-redis')
 const connections = require('kth-node-api-call').Connections
 
 const opts = {
-  log: log,
-  redis: redis,
+  log,
+  redis,
+  cache: config.cache,
+  retryOnESOCKETTIMEDOUT: true,
   timeout: 30000,
   checkAPIs: true // performs api-key checks against the apis, if a "required" check fails, the app will exit. Required apis are specified in the config
 }
