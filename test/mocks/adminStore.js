61520b64838460d89551bb5e7da75c674f5a64d3
Elena Rakhimova <elenara@kth.se>
2019-11-14 14:18:30 +0100

    changed path in test

--- test/mocks/adminStore.js
+++ test/mocks/adminStore.js
@@ -1,51 +1,51 @@
 
 const tempSaveNewImage = (imageFile, tempImagePath, isDefaultChosen) => {
-    mockAdminStore.newImageFile = imageFile
-    mockAdminStore.tempImagePath = tempImagePath
-    mockAdminStore.isDefaultChosen = isDefaultChosen
+  mockAdminStore.newImageFile = imageFile
+  mockAdminStore.tempImagePath = tempImagePath
+  mockAdminStore.isDefaultChosen = isDefaultChosen
 }
 
 const mockAdminStore = {
-    koppsData: {
-        koppsText: {
-            sv: 'Algebra och geometri',
-            en: 'Ingen information tillagd'
-        },
-        mainSubject: 'Matematik',
-        courseTitleData: {
-            course_code: 'SF1624',
-            course_title: 'Algebra och geometri',
-            course_credits: 7.5,
-            apiError: false
-        },
-        lang: 'sv',
-        langIndex: 1
+  koppsData: {
+    koppsText: {
+      sv: 'Algebra och geometri',
+      en: 'Ingen information tillagd'
     },
-    browserConfig: {
-        storageUri: ''
+    mainSubject: 'Matematik',
+    courseTitleData: {
+      course_code: 'SF1624',
+      course_title: 'Algebra och geometri',
+      course_credits: 7.5,
+      apiError: false
     },
-    sellingText: {
-        'sv': 'Svensk säljande text',
-        'en': 'English selling text'
-    },
-    paths: {
-        storage: {
-            saveFile: {
-                method: 'post',
-                uri: '/kursinfoadmin/kurser/kurs/storage/saveFile/:courseCode/:published'
-            }
-        }
-    },
-    isDefaultChosen: true,
-    tempSaveNewImage: tempSaveNewImage,
-
-    doUpsertItem (text, courseCode, imageName) {
-            return new Promise((resolve, reject) => {
-            resolve({status: 200})
-        })
+    lang: 'sv',
+    langIndex: 1
+  },
+  browserConfig: {
+    storageUri: ''
+  },
+  sellingText: {
+    'sv': 'Svensk säljande text',
+    'en': 'English selling text'
+  },
+  paths: {
+    storage: {
+      saveImage: {
+        method: 'post',
+        uri: '/kursinfoadmin/kurser/kurs/storage/saveImage/:courseCode/:published'
+      }
     }
-   
-};
+  },
+  isDefaultChosen: true,
+  tempSaveNewImage: tempSaveNewImage,
 
-export default mockAdminStore;
+  doUpsertItem (text, courseCode, imageName) {
+    return new Promise((resolve, reject) => {
+      resolve({status: 200})
+    })
+  }
+
+}
+
+export default mockAdminStore
 
