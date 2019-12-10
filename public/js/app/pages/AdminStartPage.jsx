528f6815acdcc0f60a7e79caf19d14bf02901a92
Elena Rakhimova <elenara@kth.se>
2019-12-10 13:51:29 +0100

    Refactored imports of CourseTitle -> PageTitle

--- public/js/app/pages/AdminStartPage.jsx
+++ public/js/app/pages/AdminStartPage.jsx
@@ -2,7 +2,7 @@ import React, { Component } from 'react'
 import { inject, observer} from 'mobx-react'
 import i18n from '../../../../i18n'
 
-import CourseTitle from '../components/CourseTitle'
+import PageTitle from '../components/PageTitle'
 import { Card, CardBody, CardTitle, CardText, CardFooter } from 'reactstrap'
 import KipLinkNav from '../components/KipNav'
 import AlertMsg from '../components/AlertMsg'
@@ -20,7 +20,7 @@ class AdminStartPage extends Component {
     return (
       <div key='kursinfo-container' className='kursinfo-main-page col' >
         {/* ---COURSE TITEL--- */}
-        <CourseTitle key='title'
+        <PageTitle key='title'
           courseTitleData={courseTitleData}
           pageTitle={pageTitles.administrate}
           language={lang}
