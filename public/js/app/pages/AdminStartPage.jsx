88f6b72d9acae29c092df16511a261d28d4ff3c5
Elena Rakhimova <elenara@kth.se>
2020-05-13 12:31:55 +0200

    Added card for a new kurs-pm-data-admin and entry links to it

--- public/js/app/pages/AdminStartPage.jsx
+++ public/js/app/pages/AdminStartPage.jsx
@@ -3,11 +3,11 @@ import { inject, observer} from 'mobx-react'
 import i18n from '../../../../i18n'
 
 import PageTitle from '../components/PageTitle'
-import { Card, CardBody, CardTitle, CardText, CardFooter } from 'reactstrap'
+import { Badge, Card, CardBody, CardTitle, CardText, CardFooter } from 'reactstrap'
 import KipLinkNav from '../components/KipNav'
 import AlertMsg from '../components/AlertMsg'
 
-import { ADMIN_COURSE_UTV, ADMIN_COURSE_PM, ADMIN_OM_COURSE, PM_TEMPLATE } from '../util/constants'
+import { ADMIN_COURSE_UTV, ADMIN_COURSE_PM, ADMIN_COURSE_PM_DATA, ADMIN_OM_COURSE, PM_TEMPLATE } from '../util/constants'
 
 @inject(['adminStore']) @observer
 class AdminStartPage extends Component {
@@ -47,20 +47,25 @@ class AdminStartPage extends Component {
             </Card>
             <Card>
               <CardBody>
-                <CardTitle><h4>{startCards.coursePM_hd}</h4></CardTitle>
+                <CardTitle><h4>{startCards.coursePM_hd} <Badge color="success">New</Badge></h4></CardTitle>
                 <CardText>
                   {startCards.coursePM_desc}
                 </CardText>
               </CardBody>
               <CardFooter className='text-right'>
-                <a href={PM_TEMPLATE} className='btn btn-primary' alt={startCards.coursePM_btn_template} target='_blank'>
-                  {startCards.coursePM_btn_template}
+                <a href={`${ADMIN_COURSE_PM_DATA}${courseCode}?l=${lang}`} className='btn btn-primary' alt={startCards.coursePM_btn_new}>
+                  {startCards.coursePM_btn_new}
                 </a>
-                <a href={`${ADMIN_COURSE_PM}${courseCode}?l=${lang}`} className='btn btn-primary' alt={startCards.coursePM_btn}>
-                  {startCards.coursePM_btn}
+                <a href={`${ADMIN_COURSE_PM_DATA}published/${courseCode}?l=${lang}`} className='btn btn-primary' alt={startCards.coursePM_btn_edit}>
+                  {startCards.coursePM_btn_edit}
                 </a>
               </CardFooter>
             </Card>
+
+          </span>
+        </div>
+        <div className='col'>
+          <span className='AdminPage--ShowDescription'>
             <Card className='course-development'>
               <CardBody>
                 <CardTitle><h4>{startCards.courseDev_hd}</h4></CardTitle>
@@ -75,6 +80,22 @@ class AdminStartPage extends Component {
                 </a>
                 <a href={`${ADMIN_COURSE_UTV}${courseCode}?l=${lang}&status=p&serv=admin&title=${courseTitleData.course_title}_${courseTitleData.course_credits}`} className='btn btn-primary' alt={startCards.courseDev_btn_edit}>
                   {startCards.courseDev_btn_edit}
+                </a>
+              </CardFooter>
+            </Card>
+            <Card>
+              <CardBody>
+                <CardTitle><h4>{startCards.coursePM_hd}</h4></CardTitle>
+                <CardText>
+                  {startCards.coursePM_desc}
+                </CardText>
+              </CardBody>
+              <CardFooter className='text-right'>
+                <a href={PM_TEMPLATE} className='btn btn-primary' alt={startCards.coursePM_btn_template} target='_blank'>
+                  {startCards.coursePM_btn_template}
+                </a>
+                <a href={`${ADMIN_COURSE_PM}${courseCode}?l=${lang}`} className='btn btn-primary' alt={startCards.coursePM_btn}>
+                  {startCards.coursePM_btn}
                 </a>
               </CardFooter>
             </Card>
