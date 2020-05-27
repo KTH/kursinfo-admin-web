4fe8ba3c488fdb16a741cc955ed8d3aed65be49d
Elena Rakhimova <elenara@kth.se>
2020-05-27 18:49:41 +0200

    New: display diff course memo cards depends on if course is in pilot

--- public/js/app/pages/AdminStartPage.jsx
+++ public/js/app/pages/AdminStartPage.jsx
@@ -3,7 +3,7 @@ import { inject, observer} from 'mobx-react'
 import i18n from '../../../../i18n'
 
 import PageTitle from '../components/PageTitle'
-import { Badge, Card, CardBody, CardTitle, CardText, CardFooter } from 'reactstrap'
+import { Badge, Card, CardBody, CardLink, CardTitle, CardText, CardFooter } from 'reactstrap'
 import KipLinkNav from '../components/KipNav'
 import AlertMsg from '../components/AlertMsg'
 
@@ -14,6 +14,7 @@ class AdminStartPage extends Component {
 
   render () {
     const { courseTitleData, lang } = this.props.adminStore.koppsData
+    const { isPilotCourse } = this.props.adminStore
     const courseCode = courseTitleData.course_code
     const { pageTitles, startCards } = i18n.messages[lang === 'en' ? 0 : 1]
 
@@ -30,7 +31,7 @@ class AdminStartPage extends Component {
           <AlertMsg courseCode={courseCode} props={this.props} lang={lang} translate={pageTitles} />
         </div>
         <div className='col'>
-          <span className='AdminPage--ShowDescription'>
+          <span className={isPilotCourse ? 'AdminPage--ShowDescription--PilotCourse' : 'AdminPage--ShowDescription'}>
             <Card className='KursInfo--SellingText'>
               <CardBody>
                 <CardTitle><h4>{startCards.sellingText_hd}</h4></CardTitle>
@@ -45,27 +46,50 @@ class AdminStartPage extends Component {
                 </a>
               </CardFooter>
             </Card>
-            <Card>
-              <CardBody>
-                <CardTitle><h4>{startCards.coursePM_hd} <Badge color="success">New</Badge></h4></CardTitle>
-                <CardText>
-                  {startCards.coursePM_desc}
-                </CardText>
-              </CardBody>
-              <CardFooter className='text-right'>
-                <a href={`${ADMIN_COURSE_PM_DATA}${courseCode}?l=${lang}`} className='btn btn-primary' alt={startCards.coursePM_btn_new}>
-                  {startCards.coursePM_btn_new}
-                </a>
-                <a href={`${ADMIN_COURSE_PM_DATA}published/${courseCode}?l=${lang}`} className='btn btn-primary' alt={startCards.coursePM_btn_edit}>
-                  {startCards.coursePM_btn_edit}
-                </a>
-              </CardFooter>
-            </Card>
-
-          </span>
-        </div>
-        <div className='col'>
-          <span className='AdminPage--ShowDescription'>
+            {isPilotCourse && (
+              <Card className='Skapa--Kurs-PM'>
+                <CardBody>
+                  <CardTitle><h4>{startCards.coursePM_hd} <Badge color="success">New</Badge></h4></CardTitle>
+                  <CardText>
+                    <p>{startCards.coursePM_create_desc_p1}</p>
+                    <p>{startCards.coursePM_create_desc_p2}</p>
+                    <p>{startCards.coursePM_create_desc_p3}</p>
+                  </CardText>
+                </CardBody>
+                <CardFooter className='text-right'>
+                  <span>
+                    <CardLink href={`${ADMIN_COURSE_PM_DATA}${courseCode}?l=${lang}`} className='btn btn-primary' alt={startCards.coursePM_btn_new}>
+                      {startCards.coursePM_btn_new}
+                    </CardLink>
+                    <CardLink href={`${ADMIN_COURSE_PM_DATA}published/${courseCode}?l=${lang}`} className='btn btn-primary' alt={startCards.coursePM_btn_edit}>
+                      {startCards.coursePM_btn_edit}
+                    </CardLink>
+                  </span>
+                  <span>
+                    <CardLink href={PM_TEMPLATE} alt={startCards.coursePM_link_template} target="_blank" className="external-link">
+                      {startCards.coursePM_link_template}
+                    </CardLink>
+                  </span>
+                </CardFooter>
+              </Card>
+            ) || (
+              <Card className='Ladda--Upp--Kurs-PM'>
+                <CardBody>
+                  <CardTitle><h4>{startCards.coursePM_hd}</h4></CardTitle>
+                  <CardText>
+                    {startCards.coursePM_desc}
+                  </CardText>
+                </CardBody>
+                <CardFooter className='text-right'>
+                  <a href={PM_TEMPLATE} className='btn btn-primary' alt={startCards.coursePM_btn_template} target='_blank'>
+                    {startCards.coursePM_btn_template}
+                  </a>
+                  <a href={`${ADMIN_COURSE_PM}${courseCode}?l=${lang}`} className='btn btn-primary' alt={startCards.coursePM_btn}>
+                    {startCards.coursePM_btn}
+                  </a>
+                </CardFooter>
+              </Card>
+            )}
             <Card className='course-development'>
               <CardBody>
                 <CardTitle><h4>{startCards.courseDev_hd}</h4></CardTitle>
@@ -80,22 +104,6 @@ class AdminStartPage extends Component {
                 </a>
                 <a href={`${ADMIN_COURSE_UTV}${courseCode}?l=${lang}&status=p&serv=admin&title=${courseTitleData.course_title}_${courseTitleData.course_credits}`} className='btn btn-primary' alt={startCards.courseDev_btn_edit}>
                   {startCards.courseDev_btn_edit}
-                </a>
-              </CardFooter>
-            </Card>
-            <Card>
-              <CardBody>
-                <CardTitle><h4>{startCards.coursePM_hd}</h4></CardTitle>
-                <CardText>
-                  {startCards.coursePM_desc}
-                </CardText>
-              </CardBody>
-              <CardFooter className='text-right'>
-                <a href={PM_TEMPLATE} className='btn btn-primary' alt={startCards.coursePM_btn_template} target='_blank'>
-                  {startCards.coursePM_btn_template}
-                </a>
-                <a href={`${ADMIN_COURSE_PM}${courseCode}?l=${lang}`} className='btn btn-primary' alt={startCards.coursePM_btn}>
-                  {startCards.coursePM_btn}
                 </a>
               </CardFooter>
             </Card>
