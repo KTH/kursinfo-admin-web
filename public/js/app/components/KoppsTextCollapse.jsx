08366cea18fa69bf03d010cd5dc4cd05fdd25daf
Elena Rakhimova <elenara@kth.se>
2020-09-24 23:47:45 +0200

    refactor: use details instead of reactstrap collapse

--- public/js/app/components/KoppsTextCollapse.jsx
+++ public/js/app/components/KoppsTextCollapse.jsx
@@ -1,40 +1,16 @@
-import React, { Component } from 'react'
-import { Collapse } from 'reactstrap'
+import React from 'react'
 
-class KoppsTextCollapse extends Component {
-  constructor (props) {
-    super(props)
-    this.state = {collapse: false}
-    this.toggleHeader = this.toggleHeader.bind(this)
-  }
-
-  toggleHeader () {
-    this.setState(state => ({collapse: !state.collapse}))
-  }
-
-  render () {
-    const { lang, instructions, koppsText } = this.props
-    return (
-      <div className='courseIntroTextCollapse'>
-        <h3>{instructions.langLabelText[lang]}</h3>
-        <div className='card collapsible blue'>
-          <div className='card-header' role='tab' tabIndex='0' onClick={this.toggleHeader}>
-            <h4 className='mb-0' id={'koppsShortDesc' + lang}>
-              <a className='collapse-header' href={'#koppsText' + lang} load='false' aria-expanded={this.state.collapse} aria-controls={'koppsShortDesc' + lang}>
-                {instructions.langLabelKopps[lang]}
-              </a>
-            </h4>
-          </div>
-          <Collapse isOpen={this.state.collapse} toggler={'#koppsShortDesc' + lang} aria-labelledby={'koppsShortDesc' + lang}>
-            <div className='card-body col'>
-              <span className='textBlock' dangerouslySetInnerHTML={{__html: koppsText}} />
-            </div>
-          </Collapse>
-        </div>
-        <h4>{instructions.langLabelIntro[lang]}</h4>
+const KoppsTextCollapse = ({ lang, instructions, koppsText }) => (
+  <div className="courseIntroTextCollapse">
+    <h3>{instructions.langLabelText[lang]}</h3>
+    <details>
+      <summary className="blue">{instructions.langLabelKopps[lang]}</summary>
+      <div>
+        <span className="textBlock" dangerouslySetInnerHTML={{ __html: koppsText }} />
       </div>
-    )
-  }
-}
+    </details>
+    <h4>{instructions.langLabelIntro[lang]}</h4>
+  </div>
+)
 
 export default KoppsTextCollapse
