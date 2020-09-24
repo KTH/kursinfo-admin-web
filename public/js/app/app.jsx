e3239abb7a1a0e197eb67e1ae556d2ea9c275b2c
Elena Rakhimova <elenara@kth.se>
2020-09-24 23:15:06 +0200

    lint: app.jsx

--- public/js/app/app.jsx
+++ public/js/app/app.jsx
@@ -1,41 +1,40 @@
 'use strict'
+
 import React from 'react'
 import ReactDOM from 'react-dom'
-import { Provider, inject } from 'mobx-react'
+import { Provider } from 'mobx-react'
 import { BrowserRouter, Route, Switch } from 'react-router-dom' // matchPath
-import { configure } from 'mobx'
 import { StaticRouter } from 'react-router'
 import AdminStore from './stores/AdminStore'
 import AdminStartPage from './pages/AdminStartPage'
 import CourseDescriptionEditorPage from './pages/CourseDescriptionEditorPage'
 import CourseStatisticsPage from './pages/CourseStatisticsPage'
+import '../../css/kursinfo-admin-web.scss'
 
-function appFactory () {
-  if (process.env['NODE_ENV'] !== 'production') {
-    configure({
-      isolateGlobalState: true
-    })
-  }
-
+function appFactory() {
   const adminStore = new AdminStore()
   if (typeof window !== 'undefined') {
     adminStore.initializeStore('adminStore')
   }
 
   return (
-    <Provider adminStore={adminStore} >
-      {/* <ProgressLayer> */}
-        <Switch>
-          <Route path='/kursinfoadmin/kurser/kurs/statistik' component={CourseStatisticsPage} />
-          <Route path='/kursinfoadmin/kurser/kurs/edit' component={CourseDescriptionEditorPage} />
-          <Route path='/kursinfoadmin/kurser/kurs/' component={AdminStartPage} />
-        </Switch>
-      {/* </ProgressLayer> */}
+    <Provider adminStore={adminStore}>
+      <Switch>
+        <Route
+          path="/kursinfoadmin/kurser/kurs/statistik/:courseRound"
+          component={CourseStatisticsPage}
+        />
+        <Route
+          path="/kursinfoadmin/kurser/kurs/edit/:courseCode"
+          component={CourseDescriptionEditorPage}
+        />
+        <Route path="/kursinfoadmin/kurser/kurs/:courseCode" component={AdminStartPage} />
+      </Switch>
     </Provider>
-    )
+  )
 }
 
-function staticRender (context, location) {
+function staticRender(context, location) {
   return (
     <StaticRouter location={location} context={context}>
       {appFactory()}
@@ -44,10 +43,10 @@ function staticRender (context, location) {
 }
 
 if (typeof window !== 'undefined') {
-  ReactDOM.render(<BrowserRouter>{appFactory()}</BrowserRouter>, document.getElementById('kth-kursinfo'))
+  ReactDOM.render(
+    <BrowserRouter>{appFactory()}</BrowserRouter>,
+    document.getElementById('kth-kursinfo')
+  )
 }
 
-export {
-    appFactory,
-    staticRender
-}
+export { appFactory, staticRender }
