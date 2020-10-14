523a092251ebc188c161b9a957d985c75dad1ddf
Elena Rakhimova <elenara@kth.se>
2020-10-14 19:08:20 +0200

    refactor: remove old code

--- server/blobStorage.js
+++ server/blobStorage.js
@@ -1,13 +1,4 @@
-
-const {
-  Aborter,
-  BlockBlobURL,
-  ContainerURL,
-  ServiceURL,
-  SharedKeyCredential,
-  StorageURL,
-  BlobURL
-} = require('@azure/storage-blob')
+const { BlobServiceClient } = require('@azure/storage-blob')
 
 const serverConfig = require('./configuration').server
 const log = require('kth-node-log')
@@ -17,68 +8,55 @@ if (process.env.NODE_ENV !== 'production') {
   require('dotenv').config()
 }
 
-module.exports = {
-  runBlobStorage: runBlobStorage
+const STORAGE_CONTAINER_NAME = serverConfig.fileStorage.kursinfoStorage.containerName
+
+const BLOB_SERVICE_SAS_URL = serverConfig.fileStorage.kursinfoStorage.blobServiceSasUrl
+
+const blobServiceClient = new BlobServiceClient(BLOB_SERVICE_SAS_URL)
+
+const getTodayDate = (fileDate = true) => {
+  const today = new Date()
+  const dd = String(today.getDate()).padStart(2, '0')
+  const mm = String(today.getMonth() + 1).padStart(2, '0') // January is 0!
+  const yyyy = today.getFullYear()
+  const hh = today.getHours()
+  const min = today.getMinutes()
+
+  return fileDate ? yyyy + mm + dd + '-' + hh + '-' + min : yyyy + mm + dd + '-' + hh + ':' + min
 }
 
-const STORAGE_ACCOUNT_NAME = serverConfig.fileStorage.kursinfoStorage.account
-const ACCOUNT_ACCESS_KEY = serverConfig.fileStorage.kursinfoStorage.accountKey
+async function _uploadBlob(blobName, content, fileType, metadata = {}) {
+  try {
+    const containerClient = blobServiceClient.getContainerClient(STORAGE_CONTAINER_NAME)
+    const blockBlobClient = containerClient.getBlockBlobClient(blobName)
 
-const ONE_MEGABYTE = 1024 * 1024
-const FOUR_MEGABYTES = 4 * ONE_MEGABYTE
-const ONE_MINUTE = 60 * 1000
+    log.debug(`Blobstorage - Upload block blob ${blobName} `)
+    const uploadBlobResponse = await blockBlobClient.upload(content, content.length)
 
-const credentials = new SharedKeyCredential(STORAGE_ACCOUNT_NAME, ACCOUNT_ACCESS_KEY)
-const pipeline = StorageURL.newPipeline(credentials)
-const serviceURL = new ServiceURL(`https://${STORAGE_ACCOUNT_NAME}.blob.core.windows.net`, pipeline)
+    await blockBlobClient.setHTTPHeaders({ blobContentType: fileType })
+    metadata.date = getTodayDate(false)
+    await blockBlobClient.setMetadata(metadata)
+    log.info('uploadBlobResponse', uploadBlobResponse)
+    return uploadBlobResponse
+  } catch (error) {
+    log.error('Error when uploading file in blobStorage: ' + blobName, { error })
+    return error
+  }
+}
 
-async function runBlobStorage (file, courseCode, metadata) {
+async function runBlobStorage(file, courseCode, metadata) {
   log.info('runBlobStorage: ', file, ', courseCode: ', courseCode, ', metadata: ', metadata)
-  const containerName = 'kursinfo-image-container'
+  // const containerName = 'kursinfo-image-container'
   const imageName = `Picture_by_own_choice_${courseCode}.${metadata.fileExtension}`
   const content = await sharp(file.data).resize(400, 300).toBuffer()
   const fileType = file.mimetype
-  const containerURL = ContainerURL.fromServiceURL(serviceURL, containerName)
-  const aborter = Aborter.timeout(30 * ONE_MINUTE)
 
-  const uploadResponse = await _uploadBlob(aborter, containerURL, imageName, content, fileType, metadata)
+  const uploadResponse = await _uploadBlob(imageName, content, fileType, metadata)
   log.debug(' Blobstorage - uploaded file response ', uploadResponse)
 
   return imageName
 }
 
-async function _uploadBlob (aborter, containerURL, blobName, content, fileType, metadata = {}) {
-  const blobURL = BlobURL.fromContainerURL(containerURL, blobName)
-  const blockBlobURL = BlockBlobURL.fromBlobURL(blobURL)
-  try {
-    const uploadBlobResponse = await blockBlobURL.upload(
-      aborter.none,
-      content,
-      content.length
-    )
-    log.debug(`Blobstorage - Upload block blob ${blobName} `)
-
-    await blockBlobURL.setHTTPHeaders(aborter, { blobContentType: fileType })
-    metadata['date'] = getTodayDate(false)
-    await blockBlobURL.setMetadata(
-      aborter,
-      metadata
-    )
-    log.info('uploadBlobResponse', uploadBlobResponse)
-    return uploadBlobResponse
-  } catch (error) {
-    log.error('Error when uploading file in blobStorage: ' + blobName, { error: error })
-    return error
-  }
-}
-
-const getTodayDate = (fileDate = true) => {
-  let today = new Date()
-  let dd = String(today.getDate()).padStart(2, '0')
-  let mm = String(today.getMonth() + 1).padStart(2, '0') // January is 0!
-  let yyyy = today.getFullYear()
-  let hh = today.getHours()
-  let min = today.getMinutes()
-
-  return fileDate ? yyyy + mm + dd + '-' + hh + '-' + min : yyyy + mm + dd + '-' + hh + ':' + min
+module.exports = {
+  runBlobStorage
 }
