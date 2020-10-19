943745ecb650e094c3ee2273529cf9154706143a
krifa-kth <66121594+krifa-kth@users.noreply.github.com>
2020-10-19 14:55:47 +0200

    New URL for User manual

--- public/js/app/util/constants.js
+++ public/js/app/util/constants.js
@@ -9,7 +9,7 @@ export const ADMIN_COURSE_PM_DATA = '/kursinfoadmin/kurs-pm-data/'
 export const ADMIN_OM_COURSE = '/kursinfoadmin/kurser/kurs/'
 export const ADMIN_COURSE_UTV = '/kursinfoadmin/kursutveckling/'
 export const USER_MANUAL_URL =
-  'https://intra.kth.se/utbildning/utbi/kursinformation/kursinfo-anvandarmanual'
+  'https://intra.kth.se/utbildning/utbildningsadministr/om-kursen/anvandarmanual-om-kursen'
 export const KOPPS_ADMIN_URL = 'https://app.kth.se/kopps/admin/courses/getAllVersions/'
 export const KOPPS_ADMIN_USERLIST_URL = 'https://app.kth.se/kopps/admin/userlist'
 export const KOPPS_ABOUT_URL =
