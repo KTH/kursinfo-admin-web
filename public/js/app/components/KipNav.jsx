920385d09a55ee757f0381effb07fbb24bec44ce
Elena Rakhimova <elenara@kth.se>
2020-12-01 10:56:17 +0100

    refactor: separate link and instruction text

--- public/js/app/components/KipNav.jsx
+++ public/js/app/components/KipNav.jsx
@@ -1,68 +1,59 @@
 import React from 'react'
-import { Table } from 'reactstrap'
 import {
   COURSE_INFO_URL,
-  COURSE_UTVECKLING,
   USER_MANUAL_URL,
   KOPPS_ADMIN_URL,
   KOPPS_ADMIN_USERLIST_URL,
   KOPPS_ABOUT_URL
 } from '../util/constants'
 
-const KipLinkNav = ({ courseCode, translate, lang }) => {
+const LinkToAboutCourseInformation = ({ courseCode, translate, lang }) => {
   //kursinfoadmin is on app.kth.se but student is www.kth.se
   const hostUrl = `https://${window.location.href.replace('app', 'www').split('/')[2]}`
-  const kursOmLink = `${hostUrl}${COURSE_INFO_URL}${courseCode}?l=${lang}`
-  const kutvLink = `${hostUrl}${COURSE_UTVECKLING}${courseCode}?l=${lang}`
-  const { links_to: linksToLabel } = translate
+  const aboutCourseLink = `${hostUrl}${COURSE_INFO_URL}${courseCode}?l=${lang}`
+
   return (
-    <span className="navigation row">
-      <Table className="kip-menu">
-        <tbody>
-          <tr>
-            <td colSpan="2">
-              <h4>{translate.about_course}</h4>
-              <p>
-                <a className="link-back" href={kursOmLink} alt={linksToLabel.kinfo.aAlt}>
-                  {linksToLabel.kinfo.aTitle}
-                </a>
-              </p>
-              <p>
-                <a className="link-back" href={kutvLink} alt={linksToLabel.kutv.aAlt}>
-                  {linksToLabel.kutv.aTitle}
-                </a>
-              </p>
-            </td>
-            <td className="admin-link">
-              <p>{translate.course_admin_title}</p>
-            </td>
-          </tr>
-        </tbody>
-      </Table>
-      <span className="right_intro col">
-        <p>{translate.instruction_1}</p>
-        <p>
-          {translate.instruction_kopps_1}
-          <a href={`${KOPPS_ADMIN_URL}${courseCode}`} alt={translate.instruction_kopps_alt}>
-            KOPPS{' '}
-          </a>
-          {translate.instruction_kopps_2}
-          <a href={KOPPS_ADMIN_USERLIST_URL} alt={translate.instruction_kopps_alt}>
-            {translate.instruction_kopps_3_link}
-          </a>
-          {translate.instruction_kopps_4}
-          <a href={KOPPS_ABOUT_URL} alt={translate.instruction_kopps_alt}>
-            {translate.instruction_kopps_5_link}
-          </a>
-        </p>
-        <p>
-          <a href={USER_MANUAL_URL} alt={translate.link_user_manual}>
-            {translate.link_user_manual}
-          </a>
-        </p>
-      </span>
-    </span>
+    <div className="navigation row">
+      <nav
+        className="main col"
+        aria-label={`${
+          lang === 'en' ? 'Go to About course information ' : 'Gå till Om kursen SF1624 '
+        }${courseCode}`}
+        lang={lang}
+      >
+        <a href={aboutCourseLink} className="link-back">
+          {translate.about_course}
+        </a>
+      </nav>
+    </div>
   )
 }
 
-export default KipLinkNav
+export const TextAboutRights = ({ courseCode, translate }) => (
+  // <span className=" row">
+  <div className="introduction col">
+    <p>{translate.instruction_1}</p>
+    <p>
+      {translate.instruction_kopps_1}
+      <a href={`${KOPPS_ADMIN_URL}${courseCode}`} alt={translate.instruction_kopps_alt}>
+        {'KOPPS '}
+      </a>
+      {translate.instruction_kopps_2}
+      <a href={KOPPS_ADMIN_USERLIST_URL} alt={translate.instruction_kopps_alt}>
+        {translate.instruction_kopps_3_link}
+      </a>
+      {translate.instruction_kopps_4}
+      <a href={KOPPS_ABOUT_URL} alt={translate.instruction_kopps_alt}>
+        {translate.instruction_kopps_5_link}
+      </a>
+    </p>
+    <p>
+      <a href={USER_MANUAL_URL} alt={translate.link_user_manual}>
+        {translate.link_user_manual}
+      </a>
+    </p>
+  </div>
+  // </span>
+)
+
+export default LinkToAboutCourseInformation
