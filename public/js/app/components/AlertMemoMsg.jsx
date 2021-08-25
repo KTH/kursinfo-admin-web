52f1f9e8800eb7c90f7ee01400ef65790f841bbe
Elena Rakhimova <elenara@kth.se>
2021-08-25 15:30:40 +0200

    feat: new Alert component for message if course memo is missing after course analysis saving/publishing

---
+++ public/js/app/components/AlertMemoMsg.jsx
@@ -0,0 +1,24 @@
+import React from 'react'
+import { Alert } from 'reactstrap'
+import { fetchParameters } from '../util/fetchUrlParams'
+
+const AlertMemoMsg = ({ props, translate = {}, lang = 'en' }) => {
+  const params = fetchParameters(props)
+  const { noMemo: noMemoRoundNames } = params
+  const { noMemo: noMemoHeader } = translate.alertMessages
+  const decodedRoundNames = decodeURI(noMemoRoundNames).trim()
+  return (
+    decodedRoundNames && (
+      <Alert color="info" aria-live="polite">
+        <h4>{noMemoHeader}</h4>
+        {lang === 'sv' ? (
+          <p>{`Använd funktionen “Kurs-PM” på denna sida och publicera ditt Kurs-PM för termin: ${decodedRoundNames}. Då kommer kurs-PM att visas på sidorna “Kursens utveckling” och “Kurs-PM”.`}</p>
+        ) : (
+          <p>{`Use the function “Course memo” on this page and publish your course memo for: ${decodedRoundNames}. It will be displayed on pages “Course development” and “Course memo”.`}</p>
+        )}
+      </Alert>
+    )
+  )
+}
+
+export default AlertMemoMsg
