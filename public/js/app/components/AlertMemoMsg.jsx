53171836b0eca98b70fbbbcf278b62e62b367b81
Elena Rakhimova <elenara@kth.se>
2021-08-25 15:37:20 +0200

    fix: show message only if data exist

--- public/js/app/components/AlertMemoMsg.jsx
+++ public/js/app/components/AlertMemoMsg.jsx
@@ -2,13 +2,15 @@ import React from 'react'
 import { Alert } from 'reactstrap'
 import { fetchParameters } from '../util/fetchUrlParams'
 
+// &noMemo=CSAMH%20(%20Startdatum%202012-08-24,%20Svenska%20)
 const AlertMemoMsg = ({ props, translate = {}, lang = 'en' }) => {
   const params = fetchParameters(props)
-  const { noMemo: noMemoRoundNames } = params
+  const { noMemo: noMemoRoundNames = '' } = params
   const { noMemo: noMemoHeader } = translate.alertMessages
   const decodedRoundNames = decodeURI(noMemoRoundNames).trim()
+
   return (
-    decodedRoundNames && (
+    decodedRoundNames !== '' && (
       <Alert color="info" aria-live="polite">
         <h4>{noMemoHeader}</h4>
         {lang === 'sv' ? (
