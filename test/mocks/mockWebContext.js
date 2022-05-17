9cf1055ba7fb9ca33951a866b8253e16a53bbdd0
Elena Rakhimova <elenara@kth.se>
2022-05-17 14:25:17 +0200

    test: rename store to context

---
+++ test/mocks/mockWebContext.js
@@ -0,0 +1,49 @@
+const mockWebContext = [
+  {
+    koppsData: {
+      koppsText: {
+        sv: 'Algebra och geometri',
+        en: 'Ingen information tillagd',
+      },
+      mainSubject: 'Matematik',
+      courseTitleData: {
+        course_code: 'SF1624',
+        course_title: 'Algebra och geometri',
+        course_credits: 7.5,
+        apiError: false,
+      },
+      lang: 'sv',
+      langIndex: 1,
+    },
+    browserConfig: {
+      storageUri: '',
+    },
+    sellingText: {
+      sv: 'Svensk säljande text',
+      en: 'English selling text',
+    },
+    paths: {
+      storage: {
+        saveImage: {
+          method: 'post',
+          uri: '/kursinfoadmin/kurser/kurs/storage/saveImage/:courseCode/:published',
+        },
+      },
+    },
+    isDefaultChosen: true,
+    userRoles: {
+      isCourseResponsible: true,
+      isSuperUser: false,
+      isExaminator: false,
+      isTeacher: false,
+    },
+
+    doUpsertItem: (text, courseCode, imageName) => {
+      return new Promise((resolve, reject) => {
+        resolve({ status: 200 })
+      })
+    },
+  },
+]
+
+export default mockWebContext
