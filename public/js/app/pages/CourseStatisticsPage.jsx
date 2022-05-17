fa31b24fd13781ec9e6cf64c6439afee19a50863
Elena Rakhimova <elenara@kth.se>
2022-05-17 10:49:29 +0200

    fix: CSVLink can have different from server href (blob:) after server side rendering is finished

--- public/js/app/pages/CourseStatisticsPage.jsx
+++ public/js/app/pages/CourseStatisticsPage.jsx
@@ -1,8 +1,9 @@
 /* eslint-disable react/jsx-one-expression-per-line */
 import React from 'react'
-import { CSVLink, CSVDownload } from 'react-csv'
+import { CSVLink } from 'react-csv'
 import { v4 as uuidv4 } from 'uuid'
 import { useWebContext } from '../context/WebContext'
+import { englishTexts } from '../components/statistic/StatisticsTexts'
 
 const analysisPerSchoolRows = combinedAnalysesDataPerSchool => {
   // SCHOOL: HTML Rows of a table Per SCHOOL data
@@ -100,7 +101,6 @@ const analysisPerSchoolCSV = combinedAnalysesDataPerSchool => {
     csvPerSchoolData.push([sC, schools[sC].numberOfCourses, schools[sC].numberOfUniqAnalyses])
   })
   csvPerSchoolData.push(['Total', totalNumberOfCourses, totalNumberOfAnalyses])
-  console.log(csvPerSchoolData)
   return csvPerSchoolData
 }
 
@@ -134,137 +134,9 @@ const memosPerSchoolCSV = combinedMemosDataPerSchool => {
   return csvPerSchoolData
 }
 
-const englishTexts = {
-  pageHeader: semester => `Course Information Statistics ${semester}`,
-  pageDescription: () => (
-    <>
-      <p>
-        A semester is expressed as a year followed by 1 for Spring or 2 for Autumn, for example, 20191 for Spring
-        semester 2019 or 20202 for Autumn semester 2020.
-      </p>
-      <p>Please note that this service cannot provide accurate statistics from earlier than 2019.</p>
-    </>
-  ),
-  subHeader: 'Per School',
-  subPageDescription:
-    'Course information statistics grouped by the school, presented for course analyses and course memos, respectively. You also have the possibility to export data from the tables to a csv file.',
-  courseAnalysesHeader: 'Course Analyses',
-  courseAnalysesDescription: () => (
-    <>
-      <p>
-        Column <q>Number of courses</q> holds the number of active courses for the particular school ending the given
-        semester according to KOPPS.
-      </p>
-      <p>
-        Column <q>Number of course analysis</q> holds the number of unique published course analysis for the particular
-        school the given semester. For course offerings running over several semesters the course analysis is presented
-        at the last semester of the course offerings.
-      </p>
-    </>
-  ),
-  sourceOfData: 'Source of Data',
-  courseDataApiDescription: koppsApiUrl => (
-    <p>
-      Course data is fetched from&nbsp;
-      <a href="https://www.kth.se/api/kopps/v2/apiInfo/courses">KOPPS API for Courses</a>, endpoint&nbsp;
-      <code>/api/kopps/v2/courses/offerings</code>. Data for the current page was fetched from&nbsp;
-      <a href={koppsApiUrl} target="_blank" rel="noreferrer">
-        <code>{koppsApiUrl}</code>
-      </a>
-      .
-    </p>
-  ),
-  courseAnalysesFilterDescription: semester => (
-    <p>
-      For course analyses, offerings that didn’t finish during the {semester} semester are filtered out. This is done by
-      discarding offerings that don’t meet the criteria:
-      <br />
-      <code>course.offered_semesters[&#123;last-element&#125;].semester == {semester}</code>
-    </p>
-  ),
-  courseAnalysesDataApiDescription: (kursutvecklingApiUrl, semestersInAnalyses) => (
-    <p>
-      Course analyses data is fetched from&nbsp;
-      <a href="https://github.com/KTH/kursutveckling-api" target="_blank" rel="noreferrer">
-        kursutveckling-api
-      </a>
-      , endpoint <code>/api/kursutveckling/v1/courseAnalyses/&#123;semester&#125;</code>. Data for the current page was
-      fetched from&nbsp;
-      <a href={kursutvecklingApiUrl} target="_blank" rel="noreferrer">
-        <code>{kursutvecklingApiUrl}&#123;semester&#125;</code>
-      </a>
-      , using semester(s) <code>{semestersInAnalyses.sort().join(', ')}</code>.
-    </p>
-  ),
-  exportCourseAnalysesData: 'Export course analyses data per school (csv file)',
-  courseMemosHeader: 'Course Memos',
-  courseMemosDescription: () => (
-    <>
-      <p>
-        Column <q>Number of (#) courses</q> holds the number of active courses for the particular school starting the
-        given semester according to KOPPS.
-      </p>
-      <p>
-        Columns <q># web course memos</q> and <q># PDF course memos</q> holds the number of unique published course
-        memos for the particular school in the given semester, seperated by publishing method. For course offerings
-        running over several semesters, the course memo is presented at the first semester of the course offerings.
-      </p>
-      <p>
-        Columns <q># memos published before start</q> and <q># memos published one week before start</q> holds the
-        number of course memos published at specific periods of time. The calculation uses dates in KOPPS.
-      </p>
-    </>
-  ),
-  courseMemosDataApiDescription: (kursPmApiUrl, semestersInMemos) => (
-    <p>
-      Course memo data is fetched from&nbsp;
-      <a href="https://github.com/KTH/kurs-pm-data-api" target="_blank" rel="noreferrer">
-        kurs-pm-data-api
-      </a>
-      , endpoint <code>/api/kurs-pm-data/v1/webAndPdfPublishedMemosBySemester/&#123;semester&#125;</code>. Data for the
-      current page was fetched from&nbsp;
-      <a href={kursPmApiUrl} target="_blank" rel="noreferrer">
-        <code>{kursPmApiUrl}&#123;semester&#125;</code>
-      </a>
-      , using semester(s) <code>{semestersInMemos.sort().join(', ')}</code>.
-    </p>
-  ),
-  courseMemosFilterDescription: semester => (
-    <>
-      <p>
-        For course memos, offerings that didn’t start during the {semester} semester are filtered out. This is done by
-        discarding offerings that doesn’t meet the criteria: <code>course.first_yearsemester == {semester}</code>. Date
-        used to determine if memo was published before the offering started is{' '}
-        <code>course.offered_semesters[&#123;{semester}&#125;].start_date</code>.
-      </p>
-      <p>
-        An earlier version of <i>Publish new course analysis and course data</i> had the option to upload course memos
-        together with course analyses. This option does not exist anymore, and course memos uploaded on that page are
-        filtered out.
-      </p>
-    </>
-  ),
-  exportCourseMemosData: 'Export course memos data per school (csv file)',
-  rawDataHeader: 'Raw Data',
-  rawDataDescription: () => (
-    <p>
-      Use the course information raw data to make aggregations for example departments or programs. You can export the
-      data to a csv file.
-    </p>
-  ),
-  exportRawCourseAnalysesData: 'Export raw course analyses data (csv file)',
-  exportRawCourseMemosData: 'Export raw course memos data (csv file)',
-}
-
-function CourseStatisticsPage(props) {
-  /* const testCsvData = [
-    ["firstname", "lastname", "email"],
-    ["Sam", "Tal", "samtal@kth.se"],
-    ["Raed", "Labes", "rl@kth.se"],
-    ["Yezzi", "Min", "ymin@kth.se"]
-  ] */
-
-  const [context, setContext] = useWebContext()
+function CourseStatisticsPage() {
+  const [context] = useWebContext()
+  const [finishedServerSideRendering, setFinishedServerSideRendering] = React.useState(false) // to make sure csv can use blob href and work properly
 
   const {
     semester,
@@ -392,6 +264,12 @@ function CourseStatisticsPage(props) {
     ])
   })
 
+  React.useEffect(() => {
+    let isMounted = true
+    if (isMounted) setFinishedServerSideRendering(true)
+    return () => (isMounted = false)
+  }, [])
+
   return (
     <div className="container standard department" style={{ paddingTop: '30px' }}>
       <div className="row">
@@ -415,14 +293,15 @@ function CourseStatisticsPage(props) {
               {englishTexts.courseAnalysesFilterDescription(semester)}
               {englishTexts.courseAnalysesDataApiDescription(kursutvecklingApiUrl, semestersInAnalyses)}
             </details>
-            <CSVLink
-              filename={`course-information-statistics-per-school-for-analyses-${semester}.csv`}
-              className="btn btn-primary float-right mb-2"
-              data={analysisPerSchoolCSV(combinedAnalysesDataPerSchool)}
-            >
-              {englishTexts.exportCourseAnalysesData}
-            </CSVLink>
-            {/* <CSVDownload  data={analysisPerSchoolCSV(combinedAnalysesDataPerSchool)}/> */}
+            {finishedServerSideRendering && (
+              <CSVLink
+                filename={`course-information-statistics-per-school-for-analyses-${semester}.csv`}
+                className="btn btn-primary float-right mb-2"
+                data={analysisPerSchoolCSV(combinedAnalysesDataPerSchool)}
+              >
+                {englishTexts.exportCourseAnalysesData}
+              </CSVLink>
+            )}
 
             <table className="table">
               <thead>
@@ -442,13 +321,15 @@ function CourseStatisticsPage(props) {
               {englishTexts.courseMemosFilterDescription(semester)}
               {englishTexts.courseMemosDataApiDescription(kursPmApiUrl, semestersInMemos)}
             </details>
-            <CSVLink
-              filename={`course-information-statistics-per-school-for-memos-${semester}.csv`}
-              className="btn btn-primary float-right mb-2"
-              data={memosPerSchoolCSV(combinedMemosDataPerSchool)}
-            >
-              {englishTexts.exportCourseMemosData}
-            </CSVLink>
+            {finishedServerSideRendering && (
+              <CSVLink
+                filename={`course-information-statistics-per-school-for-memos-${semester}.csv`}
+                className="btn btn-primary float-right mb-2"
+                data={memosPerSchoolCSV(combinedMemosDataPerSchool)}
+              >
+                {englishTexts.exportCourseMemosData}
+              </CSVLink>
+            )}
             <table className="table">
               <thead>
                 <tr>
@@ -466,13 +347,15 @@ function CourseStatisticsPage(props) {
             {englishTexts.rawDataDescription()}
             <details style={{ overflowX: 'auto', width: '100%' }}>
               <summary className="white">{englishTexts.courseAnalysesHeader}</summary>
-              <CSVLink
-                filename={`course-information-statistics-raw-data-with-analyses-${semester}.csv`}
-                className="btn btn-primary float-right mb-2"
-                data={csvPerDepartmentDataWithAnalyses}
-              >
-                {englishTexts.exportRawCourseAnalysesData}
-              </CSVLink>
+              {finishedServerSideRendering && (
+                <CSVLink
+                  filename={`course-information-statistics-raw-data-with-analyses-${semester}.csv`}
+                  className="btn btn-primary float-right mb-2"
+                  data={csvPerDepartmentDataWithAnalyses}
+                >
+                  {englishTexts.exportRawCourseAnalysesData}
+                </CSVLink>
+              )}
               <table className="table">
                 <thead>
                   <tr>
@@ -491,13 +374,15 @@ function CourseStatisticsPage(props) {
             </details>
             <details style={{ overflowX: 'auto', width: '100%' }}>
               <summary className="white">{englishTexts.courseMemosHeader}</summary>
-              <CSVLink
-                filename={`course-information-statistics-raw-data-with-memos-${semester}.csv`}
-                className="btn btn-primary float-right mb-2"
-                data={csvPerDepartmentDataWithMemos}
-              >
-                {englishTexts.exportRawCourseMemosData}
-              </CSVLink>
+              {finishedServerSideRendering && (
+                <CSVLink
+                  filename={`course-information-statistics-raw-data-with-memos-${semester}.csv`}
+                  className="btn btn-primary float-right mb-2"
+                  data={csvPerDepartmentDataWithMemos}
+                >
+                  {englishTexts.exportRawCourseMemosData}
+                </CSVLink>
+              )}
               <table className="table">
                 <thead>
                   <tr>
