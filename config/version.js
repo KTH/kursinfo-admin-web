f771d6f4631b59ec3c247de7c8a00bc0302fc4cd
David Forslöf <admin.dforslof@kth.se>
2023-04-26 13:36:22 +0200

    fix jenkins build id

--- config/version.js
+++ config/version.js
@@ -6,8 +6,8 @@
 module.exports = {
   gitBranch: 'NOT SET BY JENKINS',
   gitCommit: 'NOT SET BY JENKINS',
-  jenkinsBuild: 'NOT SET BY JENKINS',
+  jenkinsBuild: process.env.BUILD_ID,
   jenkinsBuildDate: 'NOT SET BY JENKINS',
   dockerName: 'NOT SET BY JENKINS',
-  dockerVersion: 'NOT SET BY JENKINS'
+  dockerVersion: process.env.BUILD_ID
 }
