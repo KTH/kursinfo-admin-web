8c464f2aadc26646f8c6a1d10a81960c46cb7e48
Benjamin Langlotz <benjala@kth.se>
2023-12-05 13:51:30 +0100

    chore(deps) bump @kth/kth-node-web-common

--- server/views/helpers/index.js
+++ server/views/helpers/index.js
@@ -35,12 +35,6 @@ registerHeaderContentHelper({
  * packaged helpers in https://github.com/KTH/kth-node-web-common/tree/master/lib/handlebars/helpers
  * Those only need to be required. Docs embedded in source.
  */
-require('@kth/kth-node-web-common/lib/handlebars/helpers/breadcrumbs')(
-  config.hostUrl,
-  'host_name',
-  '/node',
-  'site_name'
-)
 require('@kth/kth-node-web-common/lib/handlebars/helpers/contentedit')
 
 const i18n = require('../../../i18n')
