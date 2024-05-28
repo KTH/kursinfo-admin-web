86bbcdc1595279ba5e1e6adf4cdf11b957225b4c
Karl Andin <karl.andin@drakryggen.com>
2024-05-28 16:29:47 +0200

    feat(KUI-1264): cleanup of styles and adjustments to kth-style 10 for start page

--- public/js/app/pages/AdminStartPage.jsx
+++ public/js/app/pages/AdminStartPage.jsx
@@ -1,10 +1,10 @@
 import React from 'react'
 import { useSearchParams } from 'react-router-dom'
-import { Card, CardBody, CardLink, CardTitle, CardText, CardFooter } from 'reactstrap'
 import i18n from '../../../../i18n'
+import Button from '../components-shared/Button'
 import PageTitle from '../components/PageTitle'
 import KoppsErrorPage from '../components/KoppsErrorPage'
-import LinkToAboutCourseInformation, { TextAboutRights } from '../components/LinkAndInstruction'
+import { LinkToAboutCourseInformation, TextAboutRights } from '../components/LinkAndInstruction'
 import AlertMsg from '../components/AlertMsg'
 import AlertReminderMsg from '../components/AlertReminderMsg'
 import AlertReminderMsgNewPubMemo from '../components/AlertReminderMsgNewPubMemo'
@@ -33,7 +33,7 @@ function AdminStartPage() {
   }
 
   return (
-    <div className="kursinfo-main-page start-page">
+    <div className="kursinfo-main-page AdminStartPage">
       <LinkToAboutCourseInformation
         courseCode={courseCode}
         lang={lang}
@@ -41,115 +41,83 @@ function AdminStartPage() {
         publicPagesHref={publicPagesHref}
       />
 
-      {/* ---COURSE TITEL--- */}
       <PageTitle {...pageTitleProps} />
 
       <TextAboutRights lang={lang} translate={pageTitles} />
 
-      <div className="AdminPage--Alert">
-        <AlertMsg
-          courseCode={courseCode}
-          querySearchParams={fetchParameters(querySearchParams)}
-          lang={lang}
-          translate={pageTitles}
-          publicPagesHref={publicPagesHref}
-        />
-      </div>
-      <div className="AdminPage--Alert">
-        <AlertReminderMsg querySearchParams={fetchParameters(querySearchParams)} lang={lang} />
-      </div>
-      <div className="AdminPage--Alert">
-        <AlertReminderMsgNewPubMemo querySearchParams={fetchParameters(querySearchParams)} lang={lang} />
-      </div>
-      <div className="col">
-        <span className="AdminPage--ShowDescription">
-          {visibilityLevel === 'all' && (
-            <Card className="KursInfo--SellingText">
-              <CardBody>
-                <CardTitle>
-                  <h4>{startCards.sellingText_hd}</h4>
-                </CardTitle>
-                <CardText tag="span">
-                  <p>{startCards.sellingText_desc_p1}</p>
-                  <p>{startCards.sellingText_desc_p2}</p>
-                </CardText>
-              </CardBody>
-              <CardFooter className="text-right">
-                <a
-                  href={`${ADMIN_ABOUT_COURSE}edit/${courseCode}?l=${lang}`}
-                  aria-label={startCards.sellingText_btn}
-                  className="btn btn-primary"
-                >
-                  {startCards.sellingText_btn}
-                </a>
-              </CardFooter>
-            </Card>
-          )}
-          <Card className="Skapa--Kurs-PM">
-            <CardBody>
-              <CardTitle>
-                <h4>{startCards.coursePM_hd}</h4>
-              </CardTitle>
-              <CardText tag="span">
-                <p>{startCards.coursePM_create_desc_p1}</p>
-                <p>{startCards.coursePM_create_desc_p2}</p>
-                <p>{startCards.coursePM_create_desc_p3}</p>
-              </CardText>
-            </CardBody>
-            <CardFooter className="text-right">
-              <span>
-                <CardLink
-                  href={`${ADMIN_COURSE_PM_DATA}${courseCode}?l=${lang}`}
-                  className="btn btn-primary"
-                  aria-label={startCards.coursePM_btn_new}
-                >
-                  {startCards.coursePM_btn_new}
-                </CardLink>
-                <CardLink
-                  href={`${ADMIN_COURSE_PM_DATA}published/${courseCode}?l=${lang}`}
-                  className="btn btn-primary"
-                  aria-label={startCards.coursePM_btn_edit}
-                >
-                  {startCards.coursePM_btn_edit}
-                </CardLink>
-              </span>
-              <span>
-                <CardLink href={`${ADMIN_COURSE_PM}${courseCode}?l=${lang}`}>
-                  {startCards.coursePM_link_upload_memo}
-                </CardLink>
-              </span>
-            </CardFooter>
-          </Card>
-          {visibilityLevel === 'all' && (
-            <Card className="course-development">
-              <CardBody>
-                <CardTitle>
-                  <h4>{startCards.courseDev_hd}</h4>
-                </CardTitle>
-                <CardText tag="span">
-                  <p>{startCards.courseDev_decs_p1}</p>
-                  <p>{startCards.courseDev_decs_p2}</p>
-                </CardText>
-              </CardBody>
-              <CardFooter className="text-right">
-                <a
-                  href={`${ADMIN_COURSE_UTV}${courseCode}?l=${lang}&status=n&serv=admin&title=${courseTitleData.courseTitle}_${courseTitleData.courseCredits}`}
-                  className="btn btn-primary"
-                  aria-label={startCards.courseDev_btn_new}
-                >
-                  {startCards.courseDev_btn_new}
-                </a>
-                <a
-                  href={`${ADMIN_COURSE_UTV}${courseCode}?l=${lang}&status=p&serv=admin&title=${courseTitleData.courseTitle}_${courseTitleData.courseCredits}`}
-                  className="btn btn-primary"
-                  aria-label={startCards.courseDev_btn_edit}
-                >
-                  {startCards.courseDev_btn_edit}
-                </a>
-              </CardFooter>
-            </Card>
-          )}
-        </span>
+      <AlertMsg
+        courseCode={courseCode}
+        querySearchParams={fetchParameters(querySearchParams)}
+        lang={lang}
+        translate={pageTitles}
+        publicPagesHref={publicPagesHref}
+      />
+      <AlertReminderMsg querySearchParams={fetchParameters(querySearchParams)} lang={lang} />
+      <AlertReminderMsgNewPubMemo querySearchParams={fetchParameters(querySearchParams)} lang={lang} />
+
+      <div className="AdminStartPage__cardContainer">
+        {visibilityLevel === 'all' && (
+          <>
+            <div className="AdminStartPage__card">
+              <h4 className="AdminStartPage__cardTitle">{startCards.sellingText_hd}</h4>
+              <div className="AdminStartPage__cardBody">
+                <p>{startCards.sellingText_desc_p1}</p>
+                <p>{startCards.sellingText_desc_p2}</p>
+              </div>
+            </div>
+            <div className="AdminStartPage__cardFooter">
+              <Button variant="primary" href={`${ADMIN_ABOUT_COURSE}edit/${courseCode}?l=${lang}`}>
+                {startCards.sellingText_btn}
+              </Button>
+            </div>
+          </>
+        )}
+
+        <div className="AdminStartPage__card">
+          <h4 className="AdminStartPage__cardTitle">{startCards.coursePM_hd}</h4>
+          <div className="AdminStartPage__cardBody">
+            <p>{startCards.coursePM_create_desc_p1}</p>
+            <p>{startCards.coursePM_create_desc_p2}</p>
+            <p>{startCards.coursePM_create_desc_p3}</p>
+          </div>
+        </div>
+        <div className="AdminStartPage__cardFooter">
+          <Button variant="primary" href={`${ADMIN_COURSE_PM_DATA}${courseCode}?l=${lang}`}>
+            {startCards.coursePM_btn_new}
+          </Button>
+          <Button variant="primary" href={`${ADMIN_COURSE_PM_DATA}published/${courseCode}?l=${lang}`}>
+            {startCards.coursePM_btn_edit}
+          </Button>
+          <a className="AdminStartPage__uploadMemoLink" href={`${ADMIN_COURSE_PM}${courseCode}?l=${lang}`}>
+            {startCards.coursePM_link_upload_memo}
+          </a>
+        </div>
+
+        {visibilityLevel === 'all' && (
+          <>
+            <div className="AdminStartPage__card">
+              <h4 className="AdminStartPage__cardTitle">{startCards.courseDev_hd}</h4>
+              <div className="AdminStartPage__cardBody">
+                <p>{startCards.courseDev_decs_p1}</p>
+                <p>{startCards.courseDev_decs_p2}</p>
+              </div>
+            </div>
+            <div className="AdminStartPage__cardFooter">
+              <Button
+                variant="primary"
+                href={`${ADMIN_COURSE_UTV}${courseCode}?l=${lang}&status=n&serv=admin&title=${courseTitleData.courseTitle}_${courseTitleData.courseCredits}`}
+              >
+                {startCards.courseDev_btn_new}
+              </Button>
+              <Button
+                variant="primary"
+                href={`${ADMIN_COURSE_UTV}${courseCode}?l=${lang}&status=p&serv=admin&title=${courseTitleData.courseTitle}_${courseTitleData.courseCredits}`}
+              >
+                {startCards.courseDev_btn_edit}
+              </Button>
+            </div>
+          </>
+        )}
       </div>
     </div>
   )
