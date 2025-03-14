9cf7ac960665bc504f47bc181e28a5eb7d0cfa5b
amirhossein-haerian <ahae@kth.se>
2025-03-14 13:45:40 +0100

    feat(STUDOVRIGT-429): update cortina blocks

--- server/server.js
+++ server/server.js
@@ -7,6 +7,7 @@ const config = require('./configuration').server
 require('./api')
 const AppRouter = require('kth-node-express-routing').PageRouter
 const { getPaths } = require('kth-node-express-routing')
+const { cortinaMiddleware } = require('@kth/cortina-block')
 
 // Expose the server and paths
 server.locals.secret = new Map()
@@ -184,15 +185,11 @@ server.get(_addProxy('/logout'), oidc.logout)
  */
 server.use(
   config.proxyPrefixPath.uri,
-  require('@kth/kth-node-web-common/lib/web/cortina')({
-    blockUrl: config.blockApi.blockUrl,
-    proxyPrefixPath: config.proxyPrefixPath.uri,
-    hostUrl: config.hostUrl,
+  cortinaMiddleware({
+    blockApiUrl: config.blockApi.blockUrl,
     redisConfig: config.cache.cortinaBlock.redis,
-    globalLink: config.blockApi.globalLink,
-    addBlocks: config.blockApi.addBlocks,
+    blocksConfig: config.blockApi.addBlocks,
     redisKey: config.cache.cortinaBlock.redisKey,
-    useStyle10: true,
   })
 )
 
